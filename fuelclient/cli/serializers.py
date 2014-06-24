openstack-archive/fuel-web

Encode should be called only for strings
If not string provided as arg to print_to_output
it should not be encoded, and lets assume that proper unicode handling
will be done in custom print_method

Change-Id: If0c03571ccc7513fe85c38477ad9750ad9d82e97
Closes-Bug: #1330485

fuelclient/fuelclient/cli/serializers.py
@@ -63,7 +63,9 @@ class Serializer(object):
         if self.format_flags:
             self.print_formatted(formatted_data)
         else:
-            print_method(arg.encode('utf-8'))
+            if isinstance(arg, basestring):
+                arg = arg.encode('utf-8')
+            print_method(arg)
 
     def prepare_path(self, path):
         return "{0}.{1}".format(

openstack-archive/python-fuelclient

fuelclient/cli/serializers.py
@@ -63,7 +63,9 @@ class Serializer(object):
         if self.format_flags:
             self.print_formatted(formatted_data)
         else:
-            print_method(arg.encode('utf-8'))
+            if isinstance(arg, unicode):
+                arg = arg.encode('utf-8')
+            print_method(arg)
 
     def prepare_path(self, path):
         return "{0}.{1}".format(