50236eee9db23816489dc29913ea9ac792d49ffb
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-01-13 17:04:03 +0100

    Add --remove option for fuel plugins
    This option deletes the plugin that was previously installed.
    Optionally ==<version> can be specified as in:
    
    fuel plugins --remove fuel_plugin_example==1.0.0
    
    DocImpact
    related to blueprint cinder-neutron-plugins-in-fuel
    
    Change-Id: I8d8ac913c74710843c4a617fac214625af7eda1e

--- fuelclient/objects/plugins.py
+++ fuelclient/objects/plugins.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 import os
+import shutil
 import tarfile
 
 import yaml
@@ -41,8 +42,46 @@ class Plugins(base.BaseObject):
         for member_name in plugin_tar.getnames():
             if cls.metadata_config in member_name:
                 return yaml.load(plugin_tar.extractfile(member_name).read())
-        raise error.BadDataException("Tarfile {0} doesn't have {1}".format(
-            plugin_tar.name, cls.metadata_config))
+        raise error.BadDataException(
+            "Tarfile {name} doesn't have {config}".format(
+                name=plugin_tar.name, config=cls.metadata_config))
+
+    @classmethod
+    def get_plugin(cls, plugin_name, plugin_version=None):
+        """Returns plugin fetched by name and optionally by version.
+
+        If multiple plugin versions are found and version is not specified
+        error is returned.
+
+        :returns: dictionary with plugin data
+        """
+        checker = lambda p: p['name'] == plugin_name
+        if plugin_version is not None:
+            checker = lambda p: \
+                (p['name'], p['version']) == (plugin_name, plugin_version)
+        plugins = filter(checker, cls.get_all_data())
+        if len(plugins) == 0:
+            if plugin_version is None:
+                raise error.BadDataException(
+                    'Plugin {plugin_name} does not exist'.format(
+                        plugin_name=plugin_name)
+                )
+            else:
+                raise error.BadDataException(
+                    'Plugin {plugin_name}, version {plugin_version} does '
+                    'not exist'.format(
+                        plugin_name=plugin_name,
+                        plugin_version=plugin_version)
+                )
+        if len(plugins) > 1:
+            if plugin_version is None:
+                raise error.BadDataException(
+                    'Multiple versions of plugin {plugin_name} found. '
+                    'Please consider specifying a version in the '
+                    '{plugin_name}==<version> format'.format(
+                        plugin_name=plugin_name)
+                )
+        return plugins[0]
 
     @classmethod
     def add_plugin(cls, plugin_meta, plugin_tar):
@@ -67,4 +106,23 @@ class Plugins(base.BaseObject):
             cls.add_plugin(metadata, plugin_tar)
         finally:
             plugin_tar.close()
+        return resp
+
+    @classmethod
+    def remove_plugin(cls, plugin_name, plugin_version=None):
+        if not cls.validate_environment():
+            raise error.WrongEnvironmentError(
+                'Plugin can be removed only from master node.')
+        plugin = cls.get_plugin(plugin_name, plugin_version)
+
+        resp = cls.connection.delete_request(
+            cls.class_instance_path.format(**plugin)
+        )
+
+        plugin_path = os.path.join(
+            EXTRACT_PATH,
+            '{name}-{version}'.format(**plugin)
+        )
+        shutil.rmtree(plugin_path)
+
         return resp
