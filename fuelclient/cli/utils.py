openstack-archive/fuel-web

Add cli actions to sync deployment configuration
Deployment configuration will be stored in fuel library modules,
and should be uploaded to nailgun for each release, before
we will start deployment of any environment

Version of configuration files will be determined from path,
for example at the master bootstrap stage we will need to execute

fuel rel --sync-deployment-tasks --dir /etc/puppet/

Original deployment configuration will be stored in:
 /etc/puppet/2014.2-6.0/

implements blueprint granular-deployment-based-on-tasks

Change-Id: I7c441cc49afa9f0495da8c10647e4ffec1d8573a

fuelclient/fuelclient/cli/utils.py
@@ -1,0 +1,28 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+
+def find_files(dir_path, file_patterns):
+    """Returns generator where each item is a path to file, that satisfies
+    file_patterns condtion
+
+    :param dir_path: path to directory, e.g /etc/puppet/
+    :param file_patterns: iterable with file name, e.g (tasks.yaml,)
+    """
+    for root, dirs, file_names in os.walk(dir_path):
+        for file_name in file_names:
+            if file_name in file_patterns:
+                yield os.path.join(root, file_name)

openstack-archive/python-fuelclient

fuelclient/cli/utils.py
@@ -1,0 +1,28 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+
+def iterfiles(dir_path, file_patterns):
+    """Returns generator where each item is a path to file, that satisfies
+    file_patterns condtion
+
+    :param dir_path: path to directory, e.g /etc/puppet/
+    :param file_patterns: iterable with file name, e.g (tasks.yaml,)
+    """
+    for root, dirs, file_names in os.walk(dir_path):
+        for file_name in file_names:
+            if file_name in file_patterns:
+                yield os.path.join(root, file_name)