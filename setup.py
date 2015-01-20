7f61f0ea1df3307eae69ca4500731fe60c9d7121
Roman Prykhodchenko <me@romcheg.me>
2015-01-20 18:27:11 +0100

    Allow python-fuelclient to be packaged
     - Use pbr
     - Clean up extra dependencies
     - Trigger Nailgun's scripts in their own virtual
       environment
    
    Change-Id: Icbf2df4c056cc9adf6a01e67d4e06a979cefa274

--- setup.py
+++ setup.py
@@ -12,23 +12,16 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from setuptools import find_packages
-from setuptools import setup
+import setuptools
 
-setup(
-    name='fuelclient',
-    version='6.0.0',
-    description='Command line interface for Nailgun',
-    long_description="""Command line interface for Nailgun""",
-    author='Mirantis Inc.',
-    author_email='product@mirantis.com',
-    url='http://mirantis.com',
-    install_requires=['PyYAML==3.10', "argparse==1.2.1"],
-    packages=find_packages(),
-    package_data = {'': ['*.yaml']},
-    entry_points={
-        'console_scripts': [
-            'fuel = fuelclient.cli.parser:main',
-        ],
-    }
-)
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)
