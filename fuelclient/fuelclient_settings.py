eb9f0e4adde1ff8633737a0025d3730e4d8a3853
Roman Prykhodchenko <me@romcheg.me>
2015-01-24 19:44:55 +0100

    Use FuelClientSettings everywhere
    This patch moves all default settings from the code
    to the default single settings file. And modifies
    recently introduced settings manager to be able to
    also update default values from /etc/fuel/client/config.yaml
    file and from environment varables
    
    DocImpact
    Blueprint: re-thinking-fuel-client
    Change-Id: Ib73b0494cf30e75099aa7a0552745cd4adc3a07e

--- fuelclient/fuelclient_settings.py
+++ fuelclient/fuelclient_settings.py
@@ -16,6 +16,7 @@
 
 import os
 
+import six
 import yaml
 
 from fuelclient.cli import error
@@ -28,14 +29,15 @@ class FuelClientSettings(object):
     """Represents a model of Fuel Clients settings
 
     Default settigs file are distributed with the source code in
-    the <DIST_DIR>/fuelclient_settings.yaml.
+    the <DIST_DIR>/fuelclient_settings.yaml. Those settings can be
+    overriden by /etc/fuel/client/config.yaml file.
 
     User-specific settings may be stored in any YAML-formatted file
     the path to which should be supplied via the FUELCLIENT_CUSTOM_SETTINGS
     environment variable. Custom settins override the default ones.
 
-    NOTE: This is not to be confused with the API client settings which
-          requires a different configuration file.
+    Top level values may also be set as environment variables, e.g.
+    export SERVER_PORT=8080.
 
     """
     def __init__(self):
@@ -45,12 +47,14 @@ class FuelClientSettings(object):
         project_path = os.path.dirname(__file__)
         project_settings_file = os.path.join(project_path,
                                              'fuelclient_settings.yaml')
-        settings_files.append(project_settings_file)
+        external_default_settings = '/etc/fuel/client/config.yaml'
 
-        # Check whether a user specified a custom settings file
-        test_config = os.environ.get('FUELCLIENT_CUSTOM_SETTINGS')
-        if test_config:
-            settings_files.append(test_config)
+        self._add_file_if_exists(project_settings_file, settings_files)
+        self._add_file_if_exists(external_default_settings, settings_files)
+
+        # Add a custom settings file specified by user
+        self._add_file_if_exists(os.environ.get('FUELCLIENT_CUSTOM_SETTINGS'),
+                                 settings_files)
 
         self.config = {}
         for sf in settings_files:
@@ -62,11 +66,22 @@ class FuelClientSettings(object):
 
                 raise error.SettingsException(msg)
 
+        self._update_from_env()
+
+    def _add_file_if_exists(self, path_to_file, file_list):
+        if path_to_file and os.access(path_to_file, os.R_OK):
+            file_list.append(path_to_file)
+
     def _update_from_file(self, path):
         with open(path, 'r') as custom_config:
             self.config.update(
                 yaml.load(custom_config.read())
             )
+
+    def _update_from_env(self):
+        for k in six.iterkeys(self.config):
+            if k in os.environ:
+                self.config[k] = os.environ[k]
 
     def dump(self):
         return yaml.dump(self.config)
