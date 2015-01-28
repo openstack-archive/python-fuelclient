ee2bb34d6f5200949280a9e13b2e4127dbdd9c06
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-01-28 11:21:49 +0100

    Add 'notifications' argument
    This can be used for displaying and sending notifications to Fuel.
    
    Related-Bug: #1371757
    Change-Id: I6a5f05febf8f5a01a7b9415546ef56c11aedefce

---
+++ fuelclient/objects/notifications.py
@@ -0,0 +1,35 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelclient.objects import base
+
+
+class Notifications(base.BaseObject):
+
+    class_api_path = "notifications/"
+    class_instance_path = "notifications/{id}"
+
+    @classmethod
+    def send(cls, message, topic=None):
+        if topic is None:
+            topic = "done"
+
+        resp = cls.connection.post_request_raw(
+            cls.class_api_path, {
+                'message': message,
+                'topic': topic,
+            })
+        if resp.status_code != 201:
+            resp.raise_for_status()
+        return resp
