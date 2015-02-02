f2e5e8d663212d660141c6100893e9090f3a0a43
Przemyslaw Kaminski <pkaminski@mirantis.com>
2015-02-02 09:38:44 +0100

    Add 'token' option for easy token retrieval
    Just do
    fuel token
    
    You can use this in manual curl requests:
    curl '<api-url>' -H "X-Auth-Token: $(fuel token)"
    
    DocImpact
    Change-Id: I634160beedd347b90a249b073560483f33586b98

--- fuelclient/cli/actions/__init__.py
+++ fuelclient/cli/actions/__init__.py
@@ -20,6 +20,7 @@ from fuelclient.cli.actions.deploy import DeployChangesAction
 from fuelclient.cli.actions.environment import EnvironmentAction
 from fuelclient.cli.actions.fact import DeploymentAction
 from fuelclient.cli.actions.fact import ProvisioningAction
+from fuelclient.cli.actions.token import TokenAction
 from fuelclient.cli.actions.health import HealthCheckAction
 from fuelclient.cli.actions.interrupt import ResetAction
 from fuelclient.cli.actions.interrupt import StopAction
@@ -55,7 +56,8 @@ actions_tuple = (
     PluginAction,
     NodeGroupAction,
     NotificationsAction,
-    NotifyAction
+    NotifyAction,
+    TokenAction
 )
 
 actions = dict(
