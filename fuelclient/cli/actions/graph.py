bb5b36e33d9d4a4651b61704ee465b2efb29c7b8
Sebastian Kalinowski <sebastian@kalinowski.eu>
2015-02-10 13:38:06 +0100

    Add CLI for graph visualization
    * added new action 'graph' to get deployment graph in DOT format:
     - download full graph for cluster 1
       Examples:
         fuel graph --env 1 --download
         fuel graph --env 1 --download > graph.gv
    
     - it's possible to pass 'start', 'end', 'skip' to manipulate what
       include in graph
       Examples:
         fuel graph --env 1 --download --start netconfig
         fuel graph --env 1 --download --skip hiera
    
     - get graph with task's parents
       Examples:
         fuel graph --env 1 --download --parents-for post_deployment
    
     - render graph (optional)
       Examples:
         fuel graph --render path/to/graph.gv
    
       Rendering requires installed pydot or pygraphivz together with
       Graphviz app.
    * params that was used to download graph are saved as comments
      in file with graph in DOT format
    * added requests-mock for http requests mocking
    * improved readability of tasks related args:
      - removed redundant 'flag' argument causing to display arg two
        times in help
      - added metavar 'TASK' to tell better what we expect
    
    Depends on: Id6fe85efe2549a63737ad50e5e55a70a480c83ab
    Implements: blueprint granular-deployment-based-on-tasks
    
    Change-Id: I4d60d8b4c8eca53f459a5459cb3f54af09e04306

---
+++ fuelclient/cli/actions/graph.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+import sys
+
+import six
+
+from fuelclient.cli.actions import base
+import fuelclient.cli.arguments as Args
+from fuelclient.cli import error
+from fuelclient.cli import utils
+from fuelclient.objects import environment
+
+
+class GraphAction(base.Action):
+    """Manipulate deployment graph's representation."""
+
+    action_name = 'graph'
+
+    def __init__(self):
+        super(GraphAction, self).__init__()
+        self.args = (
+            Args.get_env_arg(),
+            Args.get_render_arg(
+                "Render graph from DOT to PNG"
+            ),
+            Args.get_download_arg(
+                "Download graph of specific cluster"
+            ),
+            Args.get_dir_arg(
+                "Select target dir to render graph."
+            ),
+            Args.group(
+                Args.get_skip_tasks(),
+                Args.get_tasks()
+            ),
+            Args.get_graph_endpoint(),
+            Args.get_graph_startpoint(),
+            Args.get_parents_arg(),
+        )
+        self.flag_func_map = (
+            ('render', self.render),
+            ('download', self.download),
+        )
+
+    @base.check_all("env")
+    def download(self, params):
+        """Download deployment graph to stdout
+        fuel graph --env 1 --download
+        fuel graph --env 1 --download --tasks A B C
+        fuel graph --env 1 --download --skip X Y --end pre_deployment
+        fuel graph --env 1 --download --skip X Y --start post_deployment
+
+        Sepcify output:
+        fuel graph --env 1 --download > outpup/dir/file.gv
+
+        Get parents only for task A:
+
+        fuel graph --env 1 --download --parents-for A
+        """
+        env = environment.Environment(params.env)
+
+        parents_for = getattr(params, 'parents-for')
+
+        used_params = "# params:\n"
+        for param in ('start', 'end', 'skip', 'tasks', 'parents-for'):
+            used_params += "# - {0}: {1}\n".format(param,
+                                                   getattr(params, param))
+
+        if params.tasks:
+            tasks = params.tasks
+        else:
+            tasks = env.get_tasks(
+                skip=params.skip, end=params.end, start=params.start)
+
+        dotraph = env.get_deployment_tasks_graph(tasks,
+                                                 parents_for=parents_for)
+        sys.stdout.write(six.text_type(used_params))
+        sys.stdout.write(six.text_type(dotraph))
+
+    @base.check_all("render")
+    def render(self, params):
+        """Render graph in PNG format
+
+        fuel graph --render graph.gv
+        fuel graph --render graph.gv --dir ./output/dir/
+
+        Read graph from stdin
+        some_process | fuel graph --render -
+        """
+        if params.render == '-':
+            dot_data = sys.stdin.read()
+            out_filename = 'graph.gv'
+        elif not os.path.exists(params.render):
+            raise error.ArgumentException(
+                "Input file does not exist"
+            )
+        else:
+            out_filename = os.path.basename(params.render)
+            with open(params.render, 'r') as f:
+                dot_data = f.read()
+
+        target_dir = self.full_path_directory(
+            self.default_directory(params.dir),
+            ''
+        )
+        target_file = os.path.join(
+            target_dir,
+            '{0}.png'.format(out_filename),
+        )
+        utils.render_graph(dot_data, target_file)
+        print('Graph saved in "{0}"'.format(target_file))
