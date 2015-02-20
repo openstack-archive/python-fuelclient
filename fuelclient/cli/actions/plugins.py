1106da05735b0e9b5c6084a7ff70c28d5f63ad0d
Evgeniy L <eli@mirantis.com>
2015-02-20 19:15:02 +0300

    Add 2.0.0 plugins support
    * install action now can install both .fp plugins and .rpm
    
    * remove action can remove .fp and .rpm plugins
    
    * update and downgrade actions are available for new plugin
      formats only
    
    * register action was added for the users who install plugin
      directly with yum/rpm
    
    * unregister action was added for the users who remove plugin
      directly with yum/rpm
    
    * sync action is required for those users who use yum to perform
      massive update which includes a lot of plugins
    
    * all unit tests were rewritten, now each layer
      (cli, objects, utils) is tested separately
    
    Change-Id: I2bc837fdaf356a4f4c7291cd0dbfc0a92dc433d6
    Implements: blueprint plugins-security-fixes-delivery

--- fuelclient/cli/actions/plugins.py
+++ fuelclient/cli/actions/plugins.py
@@ -12,12 +12,13 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from fuelclient.cli import error
+import fuelclient.cli.arguments as Args
 
 from fuelclient.cli.actions.base import Action
-import fuelclient.cli.arguments as Args
+from fuelclient.cli import error
 from fuelclient.cli.formatting import format_table
 from fuelclient.objects.plugins import Plugins
+from fuelclient import utils
 
 
 class PluginAction(Action):
@@ -38,53 +39,149 @@ class PluginAction(Action):
             Args.get_list_arg("List all available plugins."),
             Args.get_plugin_install_arg("Install action"),
             Args.get_plugin_remove_arg("Remove action"),
-            Args.get_force_arg("Update action"),
+            Args.get_plugin_register_arg("Register action"),
+            Args.get_plugin_unregister_arg("Unregister action"),
+            Args.get_plugin_update_arg("Update action"),
+            Args.get_plugin_downgrade_arg("Downgrade action"),
+            Args.get_plugin_sync_arg("Sync action"),
+            Args.get_force_arg("Force action"),
         ]
         self.flag_func_map = (
             ("install", self.install),
             ("remove", self.remove),
+            ("update", self.update),
+            ("downgrade", self.downgrade),
+            ("sync", self.sync),
+            ("register", self.register),
+            ("unregister", self.unregister),
             (None, self.list),
         )
 
     def list(self, params):
-        """Print all available plugins:
+        """Print all available plugins
+
                 fuel plugins
                 fuel plugins --list
         """
         plugins = Plugins.get_all_data()
         self.serializer.print_to_output(
             plugins,
-            format_table(
-                plugins,
-                acceptable_keys=self.acceptable_keys
-            )
-        )
+            format_table(plugins, acceptable_keys=self.acceptable_keys))
 
     def install(self, params):
-        """Enable plugin for environment
-            fuel plugins --install /tmp/plugin_sample.fb
+        """Install plugin archive and register in API service
+
+               fuel plugins --install plugin-name.fb
+               fuel plugins --install plugin-name-2.0-2.0.1-0.noarch.rpm
         """
-        results = Plugins.install_plugin(params.install, params.force)
+        file_path = params.install
+        self.check_file(file_path)
+        results = Plugins.install(file_path, force=params.force)
         self.serializer.print_to_output(
             results,
             "Plugin {0} was successfully installed.".format(
                 params.install))
 
     def remove(self, params):
-        """Remove plugin from environment
-            fuel plugins --remove plugin_sample
-            fuel plugins --remove plugin_sample==1.0.1
+        """Remove plugin from file system and from API service
+
+               fuel plugins --remove plugin-name==1.0.1
         """
-        s = params.remove.split('==')
-        plugin_name = s[0]
-        plugin_version = None
-        if len(s) == 2:
-            plugin_version = s[1]
-        elif len(s) > 2:
-            raise error.ArgumentException(
-                'Syntax: fuel plugins --remove fuel_plugin==1.0')
-        results = Plugins.remove_plugin(
-            plugin_name, plugin_version=plugin_version)
+        name, version = self.parse_name_version(params.remove)
+        results = Plugins.remove(name, version)
+
         self.serializer.print_to_output(
             results,
             "Plugin {0} was successfully removed.".format(params.remove))
+
+    def update(self, params):
+        """Update plugin from one minor version to another.
+           For example if there is a plugin with version 2.0.0,
+           plugin with version 2.0.1 can be used as update. But
+           plugin with version 2.1.0, cannot be used to update
+           plugin. Note that update is supported for plugins
+           beginning from package_version 2.0.0
+
+               fuel plugins --update plugin-name-2.0-2.0.1-0.noarch.rpm
+        """
+        plugin_path = params.update
+        self.check_file(plugin_path)
+        result = Plugins.update(plugin_path)
+        self.serializer.print_to_output(
+            result,
+            "Plugin {0} was successfully updated.".format(plugin_path))
+
+    def downgrade(self, params):
+        """Downgrade plugin from one minor version to another.
+           For example if there is a plugin with version 2.0.1,
+           plugin with version 2.0.0 can be used to perform downgrade.
+           Plugin with version 1.0.0, cannot be used to perform downgrade
+           plugin. Note that downgrade is supported for plugins
+           beginning from package_version 2.0.0
+
+               fuel plugins --downgrade plugin-name-2.0-2.0.1-0.noarch.rpm
+        """
+        plugin_path = params.downgrade
+        self.check_file(plugin_path)
+        result = Plugins.downgrade(plugin_path)
+        self.serializer.print_to_output(
+            result,
+            "Plugin {0} was successfully downgraded.".format(plugin_path))
+
+    def sync(self, params):
+        """Synchronise plugins on file system with plugins in
+           API service, creates plugin if it is not exists,
+           updates existent plugins
+
+               fuel plugins --sync
+        """
+        Plugins.sync()
+        self.serializer.print_to_output(
+            None, "Plugins were successfully synchronized.")
+
+    def register(self, params):
+        """Register plugin in API service
+
+               fuel plugins --register plugin-name==1.0.1
+        """
+        name, version = self.parse_name_version(params.register)
+        result = Plugins.register(name, version, force=params.force)
+        self.serializer.print_to_output(
+            result,
+            "Plugin {0} was successfully registered.".format(params.register))
+
+    def unregister(self, params):
+        """Deletes plugin from API service
+
+               fuel plugins --unregister plugin-name==1.0.1
+        """
+        name, version = self.parse_name_version(params.unregister)
+        result = Plugins.unregister(name, version)
+        self.serializer.print_to_output(
+            result,
+            "Plugin {0} was successfully unregistered."
+            "".format(params.unregister))
+
+    def parse_name_version(self, param):
+        """Takes the string and returns name and version
+
+        :param str param: string with name and version
+        :raises: error.ArgumentException if version is not specified
+        """
+        attrs = param.split('==')
+
+        if 2 < len(attrs) or len(attrs) < 2:
+            raise error.ArgumentException(
+                'Syntax: fuel plugins <action> fuel_plugin==1.0.0')
+
+        return attrs
+
+    def check_file(self, file_path):
+        """Checks if file exists
+
+        :param str file_path: path to the file
+        :raises: error.ArgumentException if file does not exist
+        """
+        if not utils.exists(file_path):
+            raise error.ArgumentException(
+                'File "{0}" does not exists'.format(file_path))
