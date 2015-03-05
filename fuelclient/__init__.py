cd5be390be3e7ffc233d71e4eb968d73d8038a31
Roman Prykhodchenko <me@romcheg.me>
2015-03-05 12:11:59 +0100

    Base API facade
    This patch adds base class for implementing facades
    that will provide access to appropriate API resources
    and encapsulate the implementation of the API wrapper.
    
    Blueprint: re-thinking-fuel-client
    Change-Id: I2a8b08204a7848dd99e3f2197be7e3fe9eb218dd

--- fuelclient/__init__.py
+++ fuelclient/__init__.py
@@ -11,6 +11,9 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+from fuelclient import v1
+
 try:
     import pkg_resources
     try:
@@ -20,3 +23,32 @@ try:
         __version__ = ""
 except ImportError:
     __version__ = ""
+
+version_map = {
+    'v1': {
+    }
+}
+
+def get_client(resource, version='v1'):
+    """Gets an API client for a resource
+
+    python-fuelclient provides access to Fuel's API
+    through a set of per-resource facades. In order to
+    get a proper facade it's necessary to specify the name
+    of the API resource and the version of Fuel's API.
+
+    :param resource: Name of the resource to get a facade for.
+    :type resource:  str
+                     Valid values are environment, node and task
+    :param version:  Version of the Fuel's API
+    :type version:   str,
+                     Available: v1. Default: v1.
+    :return:         Facade to the specified resource that wraps
+                     calls to the specified version of the API.
+
+    """
+    try:
+        return version_map[version][resource].get_client()
+    except KeyError:
+        msg = 'Cannot load API client for "{r}" in the API version "{v}".'
+        raise ValueError(msg.format(r=resource, v=version))
