7b280420ec1abf47f9fb36a6d007e7d435061ce2
Roman Prykhodchenko <me@romcheg.me>
2015-03-05 12:13:53 +0100

    Base command class
    This patch adds a base command class for performing
    Show-List-Delete operations on Fuels' objects.
    
    Blueprint: re-thinking-fuel-client
    Change-Id: I8bc1ca530a3c0126868a6137f8f3b67d0d35e89d

---
+++ fuelclient/commands/base.py
@@ -0,0 +1,103 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+
+from cliff import command
+from cliff import lister
+from cliff import show
+import six
+
+import fuelclient
+from fuelclient.common import data_utils
+
+
+VERSION = 'v1'
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseCommand(command.Command):
+    @abc.abstractproperty
+    def entity_name(self):
+        """Name of the Fuel entity."""
+        pass
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseListCommand(lister.Lister, BaseCommand):
+    """Lists all entities showing some information."""
+
+    @abc.abstractproperty
+    def columns(self):
+        """Names of columns in the resulting table."""
+        pass
+
+    def take_action(self, parsed_args):
+        client = fuelclient.get_client(self.entity_name, VERSION)
+
+        data = client.get_all()
+        data = data_utils.get_display_data_multi(self.columns, data)
+
+        return (self.columns, data)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseShowCommand(show.ShowOne, BaseCommand):
+    """Shows detailed information about the entity."""
+
+    @abc.abstractproperty
+    def columns(self):
+        """Names of columns in the resulting table."""
+        pass
+
+    def get_parser(self, prog_name):
+        parser = super(BaseShowCommand, self).get_parser(prog_name)
+
+        parser.add_argument('id', type=int,
+                            help='Id of the {} to '
+                                 'show.'.format(self.entity_name))
+
+        return parser
+
+    def take_action(self, parsed_args):
+        client = fuelclient.get_client(self.entity_name, VERSION)
+
+        data = client.get_by_id(parsed_args.id)
+        data = data_utils.get_display_data_single(self.columns, data)
+
+        return (self.columns, data)
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseDeleteCommand(BaseCommand):
+    """Deletes entity with the specified id."""
+
+    def get_parser(self, prog_name):
+        parser = super(BaseDeleteCommand, self).get_parser(prog_name)
+
+        parser.add_argument('id', type=int,
+                            help='Id of the {} to '
+                                 'delete.'.format(self.entity_name))
+
+        return parser
+
+    def take_action(self, parsed_args):
+        client = fuelclient.get_client(self.entity_name, VERSION)
+
+        client.delete_by_id(parsed_args.id)
+
+        msg = '{ent} with id {ent_id} was deleted\n'
+
+        self.app.stdout.write(msg.format(ent=self.entity_name.capitalize(),
+                                         ent_id=parsed_args.id))
