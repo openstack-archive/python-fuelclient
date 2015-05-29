5c72aefc9285959049f6d750ca2242d122f5afa6
Roman Prykhodchenko <me@romcheg.me>
2015-05-29 11:28:22 +0200

    Remove exist() calls from http client
    This patch removes exit() calls from the client
    and introduces a configurable timeout option which
    defaults to 5 seconds.
    
    DocImpact
    Closes-bug: #1454343
    
    Change-Id: I40231894c94c8fd75da609586ce72b12b1fed09a

--- fuelclient/client.py
+++ fuelclient/client.py
@@ -19,7 +19,6 @@ import requests
 from keystoneclient.v2_0 import client as auth_client
 from six.moves.urllib import parse as urlparse
 
-from fuelclient.cli.error import exceptions_decorator
 from fuelclient import fuelclient_settings
 from fuelclient.logs import NullHandler
 
@@ -46,6 +45,7 @@ class Client(object):
         self.ostf_root = urlparse.urljoin(self.root, "/ostf/")
         self.user = conf.KEYSTONE_USER
         self.password = conf.KEYSTONE_PASS
+        self.timeout = conf.TIMEOUT
         self.tenant = 'admin'
         self._keystone_client = None
         self._auth_required = None
@@ -59,11 +59,10 @@ class Client(object):
         return ''
 
     @property
-    @exceptions_decorator
     def auth_required(self):
         if self._auth_required is None:
             url = self.api_root + 'version'
-            resp = requests.get(url)
+            resp = requests.get(url, timeout=self.timeout)
             resp.raise_for_status()
 
             self._auth_required = resp.json().get('auth_required', False)
@@ -98,7 +97,6 @@ class Client(object):
         if self.debug:
             print(message)
 
-    @exceptions_decorator
     def delete_request(self, api):
         """Make DELETE request to specific API with some data
         """
@@ -109,12 +107,11 @@ class Client(object):
 
         headers = {'content-type': 'application/json',
                    'x-auth-token': self.auth_token}
-        resp = requests.delete(url, headers=headers)
+        resp = requests.delete(url, headers=headers, timeout=self.timeout)
         resp.raise_for_status()
 
         return resp.json()
 
-    @exceptions_decorator
     def put_request(self, api, data):
         """Make PUT request to specific API with some data
         """
@@ -127,7 +124,8 @@ class Client(object):
 
         headers = {'content-type': 'application/json',
                    'x-auth-token': self.auth_token}
-        resp = requests.put(url, data=data_json, headers=headers)
+        resp = requests.put(url, data=data_json,
+                            headers=headers, timeout=self.timeout)
         resp.raise_for_status()
 
         return resp.json()
@@ -147,9 +145,8 @@ class Client(object):
 
         headers = {'x-auth-token': self.auth_token}
         params = params or {}
-        return requests.get(url, params=params, headers=headers)
+        return requests.get(url, timeout=self.timeout, params=params, headers=headers)
 
-    @exceptions_decorator
     def get_request(self, api, ostf=False, params=None):
         """Make GET request to specific API
         """
@@ -174,9 +171,9 @@ class Client(object):
 
         headers = {'content-type': 'application/json',
                    'x-auth-token': self.auth_token}
-        return requests.post(url, data=data_json, headers=headers)
+        return requests.post(url, data=data_json,
+                             headers=headers, timeout=self.timeout)
 
-    @exceptions_decorator
     def post_request(self, api, data, ostf=False):
         """Make POST request to specific API with some data
         """
@@ -185,7 +182,6 @@ class Client(object):
 
         return resp.json()
 
-    @exceptions_decorator
     def get_fuel_version(self):
         return self.get_request("version")
 
