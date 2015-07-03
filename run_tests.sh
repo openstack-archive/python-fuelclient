9d941d208f43490e312e695e88224d9544fe1e46
Andriy Popovych <popovych.andrey@gmail.com>
2015-07-03 10:30:05 +0300

    Replase nosetests with pytest
    Closes-Bug: #1471815
    Change-Id: I786bc966572a3e022592a40a3b61596a6c264e54

--- run_tests.sh
+++ run_tests.sh
@@ -195,7 +195,7 @@ run_cli_tests() {
     # run tests
     NAILGUN_CONFIG=$config LISTEN_PORT=$NAILGUN_PORT \
         NAILGUN_ROOT=$NAILGUN_ROOT tox -e$env_to_run -- -vv $testropts \
-        ${certain_tests[@]} --xunit-file $FUELCLIENT_XUNIT || return 1
+        ${certain_tests[@]} --junit-xml $FUELCLIENT_XUNIT || return 1
     popd > /dev/null
 
     return 0
