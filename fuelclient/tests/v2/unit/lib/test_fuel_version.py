a3bef3b1d0d6d2e6bfc64214b137ec61efb1939b
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2015-07-15 13:56:19 +0300

    Fuel version as full action instead of option
    Implementation for both cases:
    
    - as action for fuel v1;
    - as command for fuel v2.
    
    Add deprecation message for --fuel-version
    in 7.0 release.
    
    Also solved several problems:
    
    - support custom credential;
    - support interective mode;
    - support unified scheme of api queries.
    
    Change-Id: I915d3b5ff265883883fbf92786c3cbc860ec4128
    Closes-Bug: #1474767
    Closes-Bug: #1399608
    DocImpact

---
+++ fuelclient/tests/v2/unit/lib/test_fuel_version.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import requests_mock as rm
+
+import fuelclient
+from fuelclient.tests.v2.unit.lib import test_api
+
+
+class TestFuelVersionFacade(test_api.BaseLibTest):
+
+    def setUp(self):
+        super(TestFuelVersionFacade, self).setUp()
+
+        self.version = 'v1'
+        self.res_uri = '/api/{version}/version/'.format(version=self.version)
+
+        self.client = fuelclient.get_client('fuel-version', self.version)
+
+    def test_fuel_version(self):
+        self.client.get_all()
+
+        self.assertEqual(rm.GET, self.session_adapter.last_request.method)
+        self.assertEqual(self.res_uri, self.session_adapter.last_request.path)
