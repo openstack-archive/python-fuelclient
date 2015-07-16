bb3091858b13faeadda6839ae3df79726862605c
Roman Prykhodchenko <me@romcheg.me>
2015-07-16 10:57:56 +0200

    Removed exit() calls from the library
    exit() should only be called in CLI bits of the
    client. Otherwise the library cannot be used at all.
    
    Closes-bug: #1454343
    Closes-bug: #1475642
    Change-Id: I462702faa5c09fa743de1cbcd56b87757e1095a3

--- fuelclient/tests/v2/unit/lib/test_node.py
+++ fuelclient/tests/v2/unit/lib/test_node.py
@@ -15,9 +15,12 @@
 #    under the License.
 
 import json
+import re
+
 import requests_mock as rm
 
 import fuelclient
+from fuelclient.tests import utils
 from fuelclient.tests.v2.unit.lib import test_api
 
 
@@ -29,13 +32,23 @@ class TestNodeFacade(test_api.BaseLibTest):
         self.version = 'v1'
         self.res_uri = '/api/{version}/nodes/'.format(version=self.version)
 
+        single_regexp = re.compile('{0}\d+/?$'.format(self.res_uri))
+
+        fake_nodes = [utils.get_fake_node() for i in range(5)]
+        self.multiple_matcher = self.m_request.get(self.res_uri,
+                                                   text=json.dumps(fake_nodes))
+
+        fake_node = utils.get_fake_node()
+        self.single_matcher = self.m_request.get(single_regexp,
+                                                 text=json.dumps(fake_node))
+
         self.client = fuelclient.get_client('node', self.version)
 
     def test_node_list(self):
         self.client.get_all()
 
-        self.assertEqual(rm.GET, self.session_adapter.last_request.method)
-        self.assertEqual(self.res_uri, self.session_adapter.last_request.path)
+        self.assertEqual(rm.GET, self.multiple_matcher.last_request.method)
+        self.assertEqual(self.res_uri, self.multiple_matcher.last_request.path)
 
     def test_node_show(self):
         node_id = 42
@@ -43,17 +56,21 @@ class TestNodeFacade(test_api.BaseLibTest):
 
         self.client.get_by_id(node_id)
 
-        self.assertEqual(rm.GET, self.session_adapter.last_request.method)
-        self.assertEqual(expected_uri, self.session_adapter.last_request.path)
+        self.assertEqual(rm.GET, self.single_matcher.last_request.method)
+        self.assertEqual(expected_uri, self.single_matcher.last_request.path)
 
     def test_node_vms_list(self):
         node_id = 42
         expected_uri = "/api/v1/nodes/{0}/vms_conf/".format(node_id)
 
+        fake_vms = json.dumps([{'id': 1, 'opt2': 'val2'},
+                               {'id': 2, 'opt4': 'val4'}])
+        matcher = self.m_request.get(expected_uri, text=fake_vms)
+
         self.client.get_node_vms_conf(node_id)
 
-        self.assertEqual(rm.GET, self.session_adapter.last_request.method)
-        self.assertEqual(expected_uri, self.session_adapter.last_request.path)
+        self.assertEqual(rm.GET, matcher.last_request.method)
+        self.assertEqual(expected_uri, matcher.last_request.path)
 
     def test_node_vms_create(self):
         config = [{'id': 1, 'opt2': 'val2'},
@@ -63,8 +80,11 @@ class TestNodeFacade(test_api.BaseLibTest):
         expected_uri = "/api/v1/nodes/{0}/vms_conf/".format(node_id)
         expected_body = json.dumps(config)
 
+        matcher = self.m_request.put(expected_uri,
+                                     text=json.dumps(expected_body))
+
         self.client.node_vms_create(node_id, config)
 
-        self.assertEqual(rm.PUT, self.session_adapter.last_request.method)
-        self.assertEqual(expected_uri, self.session_adapter.last_request.path)
-        self.assertEqual(expected_body, self.session_adapter.last_request.body)
+        self.assertEqual(rm.PUT, matcher.last_request.method)
+        self.assertEqual(expected_uri, matcher.last_request.path)
+        self.assertEqual(expected_body, matcher.last_request.body)
