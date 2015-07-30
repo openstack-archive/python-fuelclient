18aff7c9e681f0f9c2a1726c829cead2d18f8d03
Alexander Saprykin <asaprykin@mirantis.com>
2015-07-30 13:56:38 +0300

    Add network groups support for v2
    Examples:
    
    To create a new network group:
        fuel2 network-group create --node-group 1 --release 2 --vlan 100
            --cidr 10.0.0.0/24 New_Network
    
    To update network group:
        fuel2 network-group update --name New_Name --vlan 200 1
    
    To delete specified network group:
        fuel2 network-group delete 1
    
    To list all available network groups:
        fuel2 network-group list
    
    To show specified network group:
        fuel2 network-group show 1
    
    Partial-Bug: #1478577
    Change-Id: I9eaa01af25b68c6d41b02c7d9d99ae42ce998515

--- fuelclient/v1/__init__.py
+++ fuelclient/v1/__init__.py
@@ -14,6 +14,7 @@
 
 from fuelclient.v1 import environment
 from fuelclient.v1 import fuelversion
+from fuelclient.v1 import network_group
 from fuelclient.v1 import node
 from fuelclient.v1 import task
 
@@ -21,5 +22,6 @@ from fuelclient.v1 import task
 # Please keeps the list in alphabetical order
 __all__ = ('environment',
            'fuelversion',
+           'network_group',
            'node',
            'task')
