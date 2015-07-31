78c8f27579945fe4c7a8979639adbcd4659a9985
Alexander Saprykin <asaprykin@mirantis.com>
2015-07-31 12:46:57 +0300

    Add network templates support for v2
    Partial-Bug: #1478577
    Change-Id: I76a5dc887ee50c9482fdd847fa55968a9727b02d

---
+++ fuelclient/commands/network_template.py
@@ -0,0 +1,101 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelclient.commands import base
+
+
+class NetworkTemplateMixin(object):
+
+    entity_name = 'environment'
+
+    @staticmethod
+    def add_env_argument(parser):
+        parser.add_argument(
+            '-e', '--env',
+            type=int,
+            required=True,
+            help='Upload network template for specified environment'
+        )
+
+    @staticmethod
+    def add_dir_argument(parser):
+        parser.add_argument(
+            '-d', '--dir',
+            type=str,
+            required=True,
+            help='Directory with network data'
+        )
+
+
+class NetworkTemplateUpload(NetworkTemplateMixin, base.BaseCommand):
+    """To upload network configuration for specified environment:
+
+        fuel2 network-template upload --env 1 --dir path/to/directory
+    """
+
+    def get_parser(self, prog_name):
+        parser = super(NetworkTemplateUpload, self).get_parser(prog_name)
+
+        self.add_dir_argument(parser)
+        self.add_env_argument(parser)
+
+        return parser
+
+    def take_action(self, parsed_args):
+        self.client.upload_network_template(parsed_args.env, parsed_args.dir)
+        msg = "Network configuration uploaded\n"
+        self.app.stdout.write(msg)
+
+
+class NetworkTemplateDownload(NetworkTemplateMixin, base.BaseCommand):
+    """To download network configuration for environment to the specified
+    directory:
+        fuel2 network-template download --env 1 --dir path/to/directory
+    """
+
+    def get_parser(self, prog_name):
+        parser = super(NetworkTemplateDownload, self).get_parser(prog_name)
+
+        self.add_dir_argument(parser)
+        self.add_env_argument(parser)
+
+        return parser
+
+    def take_action(self, parsed_args):
+        network_file_path = self.client.download_network_template(
+            parsed_args.env, parsed_args.dir)
+
+        msg = ("Network configuration for environment with id={0}"
+               " downloaded to {1}\n").format(
+                   parsed_args.env, network_file_path)
+        self.app.stdout.write(msg)
+
+
+class NetworkTemplateVerify(NetworkTemplateMixin, base.BaseCommand):
+    """To verify network configuration for specified environment:
+            fuel2 network-template verify --env 1
+    """
+
+    def get_parser(self, prog_name):
+        parser = super(NetworkTemplateVerify, self).get_parser(prog_name)
+
+        self.add_env_argument(parser)
+
+        return parser
+
+    def take_action(self, parsed_args):
+        response = self.client.verify_network_template(parsed_args.env)
+
+        msg = "Verification status is '{status}'. Message: {message}\n"
+        self.app.stdout.write(msg.format(**response))
