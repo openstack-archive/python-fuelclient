c77841e6e6bfdc0c6e112b30ce76a1c7084b0bc2
Andrzej Skupien <askupien@mirantis.com>
2015-08-04 11:12:44 +0200

    Nodegroup can be created not only for GRE
    Before: Nodegroup creation fails for non-GRE environmets with
    an error
    "403 Client Error: Forbidden (Node groups can only be created
    when using Neutron GRE.)"
    
    After: Nodegroup is created for non-GRE environmets with a
    warning:
    "WARNING: Node groups should be used for OpenStack environments
     that use an encapsulation protocol such as Neutron GRE."
    
    Closes-Bug: #1472322
    Depends-On: Iaa35fe16a02d73ced7a87a0c7ac2d167d1e77740
    Change-Id: I56d70e7ca21b47f161dd85108c09ad54186f872e
    Signed-off-by: Andrzej Skupien <askupien@mirantis.com>

--- fuelclient/tests/v1/unit/test_nodegroups.py
+++ fuelclient/tests/v1/unit/test_nodegroups.py
@@ -13,35 +13,50 @@
 #    under the License.
 
 import mock
+import requests_mock
 
 from fuelclient.tests import base
 
 
-@mock.patch('fuelclient.client.requests')
+@requests_mock.mock()
 class TestNodeGroupActions(base.UnitTestCase):
 
+    def setUp(self):
+        super(TestNodeGroupActions, self).setUp()
+
+        self.env_id = 42
+        self.req_base_path = '/api/v1/nodegroups/'
+
     def test_list_nodegroups(self, mreq):
+        mreq.get(self.req_base_path)
         self.execute(['fuel', 'nodegroup', '--list'])
-        call_args = mreq.get.call_args_list[-1]
-        url = call_args[0][0]
-        self.assertIn('nodegroups/', url)
+        self.assertEqual(mreq.last_request.method, 'GET')
+        self.assertEqual(mreq.last_request.path, self.req_base_path)
 
     def test_create_nodegroup(self, mreq):
+        mreq.post(self.req_base_path)
         self.execute(['fuel', 'nodegroup', '--create',
-                      '--name', 'test group', '--env', '1'])
+                      '--name', 'test group', '--env', str(self.env_id)])
 
-        call_args = mreq.post.call_args_list[0]
-        call_data = call_args[-1]['data']
-        url = call_args[0][0]
-        self.assertIn('"cluster_id": 1', call_data)
-        self.assertIn('"name": "test group"', call_data)
-        self.assertIn('nodegroups/', url)
+        call_data = mreq.last_request.json()
+        self.assertEqual(self.env_id, call_data['cluster_id'])
+        self.assertEqual('test group', call_data['name'])
+
+        self.assertEqual(mreq.last_request.method, 'POST')
+        self.assertEqual(mreq.last_request.path, self.req_base_path)
 
     def test_delete_nodegroup(self, mreq):
-        self.execute(['fuel', 'nodegroup', '--delete', '--group', '3'])
-        call_args = mreq.delete.call_args_list[-1]
-        url = call_args[0][0]
-        self.assertIn('nodegroups/3', url)
+        path = self.req_base_path + str(self.env_id) + '/'
+        mreq.get(path, json={'name': 'test group'})
+        delete_path = self.req_base_path + str(self.env_id) + '/'
+        mreq.delete(delete_path)
+        self.execute(['fuel', 'nodegroup', '--delete', '--group',
+                      str(self.env_id)])
+        self.assertEqual(mreq.request_history[-2].method, 'GET')
+        self.assertEqual(mreq.request_history[-2].path, path)
+
+        self.assertEqual(mreq.last_request.method, 'DELETE')
+        self.assertEqual(mreq.last_request.path, delete_path)
 
     def test_assign_nodegroup_fails_w_multiple_groups(self, mreq):
         err_msg = "Nodes can only be assigned to one node group.\n"
@@ -54,7 +69,7 @@ class TestNodeGroupActions(base.UnitTestCase):
         self.assertEqual(msg, err_msg)
 
     @mock.patch('fuelclient.objects.nodegroup.NodeGroup.assign')
-    def test_assign_nodegroup(self, m_assign, mreq):
+    def test_assign_nodegroup(self, m_req, m_assign):
         self.execute(['fuel', 'nodegroup', '--assign', '--node', '1',
                       '--env', '1', '--group', '2'])
         m_assign.assert_called_with([1])
