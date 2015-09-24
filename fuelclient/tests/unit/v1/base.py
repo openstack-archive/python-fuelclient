3e2c7a9322f30fcb19bb4776118274eb0aae6aa0
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2015-09-24 13:16:25 +0200

    Change node name from CLI (fuel1)
    Added possibility to change node name:
    `fuel node --node-id --name NewName`
    The functionality for fuel2 has been already merged
    in separate patch.
    
    Also unit tests added.
    
    Change-Id: I6d3384d1615bdd1dbb53508a0c2908e62f05898b
    Partial-Bug: #1498523
    (cherry picked from commit 6810231ec6d333fc10f7020f9bdf5122e05e998a)

---
+++ fuelclient/tests/unit/v1/base.py
@@ -0,0 +1,72 @@
+#    Copyright 2013-2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+from oslotest import base as oslo_base
+import requests_mock as rm
+import six
+
+from fuelclient.cli import parser
+
+
+class FakeFile(six.StringIO):
+    """Context manager for a fake file
+
+    NOTE(eli): We cannot use mock_open from mock library
+    here, because it hangs when we use 'with' statement,
+    and when we want to read file by chunks.
+
+    """
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        pass
+
+
+class UnitTestCase(oslo_base.BaseTestCase):
+    """Base test class which does not require nailgun server to run."""
+
+    def setUp(self):
+        super(UnitTestCase, self).setUp()
+
+        self.auth_required_patcher = mock.patch('fuelclient.client.'
+                                                'Client.auth_required',
+                                                new_callable=mock.PropertyMock)
+
+        self.auth_required_mock = self.auth_required_patcher.start()
+        self.auth_required_mock.return_value = False
+
+        self.m_request = rm.Mocker()
+        self.m_request.start()
+
+        self.addCleanup(self.auth_required_patcher.stop)
+        self.addCleanup(self.m_request.stop)
+
+    def execute(self, command):
+        """Execute old CLI."""
+
+        return parser.main(command)
+
+    def mock_open(self, text, filename='some.file'):
+        """Mocks builtin open function
+
+        Usage example:
+
+          with mock.patch('__builtin__.open', self.mock_open('file content')):
+              # call mocked code
+        """
+        fileobj = FakeFile(text)
+        setattr(fileobj, 'name', filename)
+        return mock.MagicMock(return_value=fileobj)
