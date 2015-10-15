71de29a7f65b481389067281023d4e2a55136f6f
Roman Prykhodchenko <me@romcheg.me>
2015-10-15 10:59:51 +0200

    Generate user settings file
    Since relying on a system-wide /etc/... configuration
    file is not a good approach for a user-side application
    it's necessary to not use that file by default.
    
    This patch changes default settings file to ~/.config/fuel_client.yaml
    and adds generation of one with default values if it does not exist.
    Specifying of a custom settings file using FUELCLIENT_CUSTOM_SETTINGS
    variable is still possible.
    
    DocImpact
    Closes-bug: #1458361
    Change-Id: Ia17c8edbdd71a74318d3237a47d4b5650934c378

--- fuelclient/tests/unit/common/test_settings.py
+++ fuelclient/tests/unit/common/test_settings.py
@@ -1,112 +1,28 @@
-# -*- coding: utf-8 -*-
-#
-#    Copyright 2015 Mirantis, Inc.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+import os
 
-from mock import mock_open
-from mock import patch
+import mock
 
+from fuelclient import fuelclient_settings
 from fuelclient.tests.unit.v1 import base
 
 
-YAML_SETTINGS_DATA = """editable:
-  access:
-    user:
-      value: test_user
-"""
-JSON_SETTINGS_DATA = {
-    'editable': {
-        'access': {
-            'user': {
-                'value': 'test_user'
-            }
-        }
-    }
-}
+class TestSettings(base.UnitTestCase):
 
+    @mock.patch('shutil.copy')
+    @mock.patch('os.chmod')
+    @mock.patch('os.path.exists')
+    def test_config_generation(self, m_exists, m_chmod, m_copy):
+        project_dir = os.path.dirname(fuelclient_settings.__file__)
 
-class BaseSettings(base.UnitTestCase):
+        expected_mode = 0o600
+        expected_default = os.path.join(project_dir,
+                                        'fuelclient_settings.yaml')
+        expected_path = os.path.expanduser('~/.config/fuel_client.yaml')
 
-    def check_upload_action(self, test_command, test_url):
-        m = mock_open(read_data=YAML_SETTINGS_DATA)
-        put = self.m_request.put(test_url, json={})
+        fuelclient_settings._SETTINGS = None
+        m_exists.return_value = False
 
-        with patch('six.moves.builtins.open', m, create=True):
-            self.execute(test_command)
+        fuelclient_settings.get_settings()
 
-        m().read.assert_called_once_with()
-        self.assertTrue(put.called)
-        self.assertDictEqual(put.last_request.json(), JSON_SETTINGS_DATA)
-
-    def check_default_action(self, test_command, test_url):
-        m = mock_open()
-        get = self.m_request.get(test_url, json=JSON_SETTINGS_DATA)
-
-        with patch('six.moves.builtins.open', m, create=True):
-            self.execute(test_command)
-
-        self.assertTrue(get.called)
-        m().write.assert_called_once_with(YAML_SETTINGS_DATA)
-
-    def check_download_action(self, test_command, test_url):
-        m = mock_open()
-        get = self.m_request.get(test_url, json=JSON_SETTINGS_DATA)
-
-        with patch('six.moves.builtins.open', m, create=True):
-            self.execute(test_command)
-
-        m().write.assert_called_once_with(YAML_SETTINGS_DATA)
-        self.assertTrue(get.called)
-
-
-class TestSettings(BaseSettings):
-
-    def test_upload_action(self):
-        self.check_upload_action(
-            test_command=[
-                'fuel', 'settings', '--env', '1', '--upload'],
-            test_url='/api/v1/clusters/1/attributes')
-
-    def test_default_action(self):
-        self.check_default_action(
-            test_command=[
-                'fuel', 'settings', '--env', '1', '--default'],
-            test_url='/api/v1/clusters/1/attributes/defaults')
-
-    def test_download_action(self):
-        self.check_download_action(
-            test_command=[
-                'fuel', 'settings', '--env', '1', '--download'],
-            test_url='/api/v1/clusters/1/attributes')
-
-
-class TestVmwareSettings(BaseSettings):
-
-    def test_upload_action(self):
-        self.check_upload_action(
-            test_command=[
-                'fuel', 'vmware-settings', '--env', '1', '--upload'],
-            test_url='/api/v1/clusters/1/vmware_attributes')
-
-    def test_default_action(self):
-        self.check_default_action(
-            test_command=[
-                'fuel', 'vmware-settings', '--env', '1', '--default'],
-            test_url='/api/v1/clusters/1/vmware_attributes/defaults')
-
-    def test_download_action(self):
-        self.check_download_action(
-            test_command=[
-                'fuel', 'vmware-settings', '--env', '1', '--download'],
-            test_url='/api/v1/clusters/1/vmware_attributes')
+        m_copy.assert_called_once_with(expected_default, expected_path)
+        m_chmod.assert_called_once_with(expected_path, expected_mode)
