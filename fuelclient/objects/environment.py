354a3c0d9a16928b2c971351c5d64e51cd786041
Roman Prykhodchenko <me@romcheg.me>
2015-11-08 19:20:22 +0100

    Remove nova network support
    nova-network was deprecated in Fuel 6.1 and was locked
    as an option in Fuel UI. This patch removes deprecated
    functionality.
    
    Closes-bug: #1514212
    Change-Id: Ic9e5628861d3b96bd6122de587fb223fdcc4f238

--- fuelclient/objects/environment.py
+++ fuelclient/objects/environment.py
@@ -35,7 +35,7 @@ class Environment(BaseObject):
     network_template_path = 'clusters/{0}/network_configuration/template'
 
     @classmethod
-    def create(cls, name, release_id, net, net_segment_type):
+    def create(cls, name, release_id, net_segment_type):
         data = {
             "nodes": [],
             "tasks": [],
@@ -43,11 +43,6 @@ class Environment(BaseObject):
             "release_id": release_id,
             "net_segment_type": net_segment_type,
         }
-
-        if net.lower() == "nova":
-            data["net_provider"] = "nova_network"
-        else:
-            data["net_provider"] = "neutron"
 
         data = cls.connection.post_request("clusters/", data)
         return cls.init_with_data(data)
@@ -236,7 +231,7 @@ class Environment(BaseObject):
 
     @property
     def network_url(self):
-        return "clusters/{id}/network_configuration/{net_provider}".format(
+        return "clusters/{id}/network_configuration/neutron".format(
             **self.data
         )
 
--- fuelclient/tests/unit/v1/test_release_networks_action.py
+++ fuelclient/tests/unit/v1/test_release_networks_action.py
@@ -20,8 +20,8 @@ import requests_mock as rm
 from fuelclient.tests.unit.v1 import base
 
 
-API_INPUT = {'config': 'nova_network'}
-API_OUTPUT = 'config: nova_network\n'
+API_INPUT = {'config': 'neutron'}
+API_OUTPUT = 'config: neutron\n'
 
 
 @patch('fuelclient.cli.serializers.open', create=True)
