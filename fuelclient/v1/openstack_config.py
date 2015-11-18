cd7e0fded92fbac57ffde5f11d26228bf647f47d
Alexander Saprykin <asaprykin@mirantis.com>
2015-11-18 16:58:37 +0100

    Implement CLI for openstack configuration
    Add openstack-config commands for fuel and fuel2
    Examples for fuel:
    
    fuel openstack-config --env 1 --list
    fuel openstack-config --env 1 [--node 1 | --role controller] --upload
        --file config.yaml
    fuel openstack-config --config 1 --download --file config.yaml
    fuel openstack-config --env 1 [--node 1 | --role controller] --execute
    
    Examples for fuel2:
    
    fuel2 openstack-config list --env 1
    fuel2 openstack-config upload --env 1 [--node 1 | --role controller]
        --file config.yaml
    fuel2 openstack-config download 1 --file config.yaml
    fuel2 openstack-config execute --env 1 [--node 1 | --role controller]
    
    Change-Id: Id701d4b8408bd275ac6e7bc53ca23b9f3deb4f6d
    Implements: blueprint openstack-config-change

---
+++ fuelclient/v1/openstack_config.py
@@ -0,0 +1,45 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelclient import objects
+from fuelclient.v1 import base_v1
+
+
+class OpenstackConfigClient(base_v1.BaseV1Client):
+
+    _entity_wrapper = objects.OpenstackConfig
+
+    def upload(self, path, cluster_id, node_id=None, node_role=None):
+        config = self._entity_wrapper.read_file(path)
+        self._entity_wrapper.create(
+            cluster_id=cluster_id, config=config,
+            node_id=node_id, node_role=node_role)
+
+        return path
+
+    def download(self, config_id, path):
+        config = self._entity_wrapper(config_id)
+        config.write_file(path, config.data['config'])
+
+        return path
+
+    def execute(self, **kwargs):
+        return self._entity_wrapper.execute(**kwargs)
+
+    def get_filtered(self, **kwargs):
+        return self._entity_wrapper.get_filtered_data(**kwargs)
+
+
+def get_client():
+    return OpenstackConfigClient()
