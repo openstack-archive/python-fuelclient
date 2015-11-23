d0e725021b2f029e21dc930f501b358880be9f3a
Aleksandr Didenko <adidenko@mirantis.com>
2015-11-23 14:48:03 +0100

    Change cluster to cluster_id in nodegroup
    Introduced by change-id: I413f77ce1dca59ae2c48ba743513ddf5584713e8
    Nodegroup 'cluster' field has been changed to 'cluster_id' in
    nailgun. So we need to adjust fuelclient accordingly.
    
    Also includes a minor change in nodegroups unit tests.
    
    Change-Id: Iba3df02142279c14910578770187ac93157190c4
    Closes-bug: #1518341

--- fuelclient/tests/unit/v1/test_nodegroups.py
+++ fuelclient/tests/unit/v1/test_nodegroups.py
@@ -17,7 +17,7 @@ from six import StringIO
 
 from fuelclient.tests.unit.v1 import base
 from fuelclient.tests.utils import fake_env
-from fuelclient.tests.utils import fake_network_group
+from fuelclient.tests.utils import fake_node_group
 
 
 class TestNodeGroupActions(base.UnitTestCase):
@@ -27,7 +27,7 @@ class TestNodeGroupActions(base.UnitTestCase):
 
         self.env = fake_env.get_fake_env()
         self.req_base_path = '/api/v1/nodegroups/'
-        self.ng = fake_network_group.get_fake_network_group()
+        self.ng = fake_node_group.get_fake_node_group()
 
     def test_list_nodegroups(self):
         mget = self.m_request.get(self.req_base_path, json=[])
