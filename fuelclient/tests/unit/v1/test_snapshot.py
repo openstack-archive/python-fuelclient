a257538e960ecaa75ecb51e05b894c6c36f12e3b
Artem Roma <aroma@mirantis.com>
2016-02-03 14:32:15 +0200

    Ignore snapshot downloading if snapshot generating task failed
    Now if generating snapshot task failed the appropriate message will be
    displayed to user and there will be no attempt of the snapshot
    downloading.
    
    Change-Id: I4a4754916c43efc2acb06f40cf2dc1df6bb4e9f5
    Closes-Bug: #1540441

--- fuelclient/tests/unit/v1/test_snapshot.py
+++ fuelclient/tests/unit/v1/test_snapshot.py
@@ -14,12 +14,19 @@
 
 import mock
 from mock import call
+from mock import Mock
 from mock import patch
 
 from fuelclient.tests.unit.v1 import base
 
 
 class TestSnapshot(base.UnitTestCase):
+
+    def setUp(self):
+        super(TestSnapshot, self).setUp()
+
+        self.mtask = Mock(status='ready', data={'message': ''})
+        self.mtask.connection = Mock(root='')
 
     @patch('fuelclient.cli.actions.snapshot.SnapshotTask.get_default_config')
     @patch('sys.stdout')
@@ -42,6 +49,8 @@ class TestSnapshot(base.UnitTestCase):
         mstdin.isatty.return_value = False
         mstdin.read.return_value = conf
 
+        mstart.return_value = self.mtask
+
         self.execute(['fuel', 'snapshot'])
 
         mstart.assert_called_once_with({'key': 'value'})
@@ -62,6 +71,8 @@ class TestSnapshot(base.UnitTestCase):
 
         mstdin.isatty.return_value = True
 
+        mstart.return_value = self.mtask
+
         self.execute(['fuel', 'snapshot'])
 
         mstart.assert_called_once_with({})
@@ -69,3 +80,21 @@ class TestSnapshot(base.UnitTestCase):
             mock.ANY,
             auth_token='token123',
             directory='.')
+
+    @patch('fuelclient.cli.actions.snapshot.SnapshotTask.start_snapshot_task')
+    @patch('sys.stdin')
+    @patch('sys.stderr')
+    def test_get_snapshot_when_task_is_failed(self, mstderr, mstdin, mstart):
+        mstdin.isatty.return_value = True
+
+        mdata = {'message': 'mock task message'}
+        self.mtask.status = 'error'
+        self.mtask.data = mdata
+
+        mstart.return_value = self.mtask
+
+        self.execute(['fuel', 'snapshot'])
+
+        err_msg = ("Snapshot generating task ended with error. "
+                   "Task message: {0}".format(mdata['message']))
+        mstderr.write.called_once_with(err_msg)
