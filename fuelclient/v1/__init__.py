90b7d3c9220b82ed9bc5cc2d17d4897dab51a979
Ilya Kutukov <ikutukov@mirantis.com>
2016-02-09 03:39:40 +0300

    Basic VIP management commands added to Nailgun CLI v2
    Now Naigun CLI v2 supports VIP configuration management commands
    that allow to download and upload VIP configuration:
    
    fuel vip download --env 1 --ip 1 --file firstvip.yaml
    fuel vip upload --env 1 --file ip_address.yaml
    
    Partial-Bug: #1482399
    Implements Blueprint: allow-any-vip
    
    Change-Id: Ib9b15bfdb7d4514919efbac2dab4416d088aa1e9

--- fuelclient/v1/__init__.py
+++ fuelclient/v1/__init__.py
@@ -17,8 +17,9 @@ from fuelclient.v1 import fuelversion
 from fuelclient.v1 import network_group
 from fuelclient.v1 import node
 from fuelclient.v1 import openstack_config
-from fuelclient.v1 import task
 from fuelclient.v1 import plugins
+from fuelclient.v1 import task
+from fuelclient.v1 import vip
 
 # Please keeps the list in alphabetical order
 __all__ = ('environment',
@@ -27,4 +28,5 @@ __all__ = ('environment',
            'node',
            'openstack_config',
            'plugins',
-           'task',)
+           'task',
+           'vip')
