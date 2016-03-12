dfa8fa7c41d0b41bebe5a26fbe8bb337acb98b01
Roman Podoliaka <rpodolyaka@mirantis.com>
2016-03-12 16:18:29 +0200

    fuel2: add --ansible-inventory option to node listing
    This option makes fuel2 client generate an ansible inventory file
    when listing cluster nodes. Nodes are grouped by roles, if a
    particular node has more than one role, it will appear in the
    inventory file several times.
    
    More details on the ansible inventory file format:
    
       http://docs.ansible.com/ansible/intro_inventory.html
    
    This could be useful when managing Fuel deployed clusters by the
    means of ansible.
    
    Change-Id: I2bcb5756ccbeae96f3614e1bda3caf7c8289973f

--- fuelclient/tests/unit/v2/cli/test_node.py
+++ fuelclient/tests/unit/v2/cli/test_node.py
@@ -14,6 +14,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import cStringIO
+
 import mock
 import six
 
@@ -75,6 +77,57 @@ class TestNodeCommand(test_engine.BaseCLITest):
             environment_id=env_id, labels=labels)
         self.assertIsInstance(
             self.m_client.get_all.call_args[1].get('labels')[0], six.text_type)
+
+    def test_node_list_ansible_inventory(self):
+        self.m_client.get_all.return_value = [
+            fake_node.get_fake_node(hostname='node-1',
+                                    ip='10.20.0.2',
+                                    roles=['compute']),
+            fake_node.get_fake_node(hostname='node-2',
+                                    ip='10.20.0.3',
+                                    roles=['compute', 'ceph-osd']),
+            fake_node.get_fake_node(hostname='node-3',
+                                    ip='10.20.0.4',
+                                    roles=['controller']),
+            fake_node.get_fake_node(hostname='node-4',
+                                    ip='10.20.0.5',
+                                    roles=['controller', 'mongo']),
+            fake_node.get_fake_node(hostname='node-5',
+                                    ip='10.20.0.6',
+                                    roles=['controller', 'ceph-osd']),
+        ]
+
+        expected_output = '''\
+[ceph-osd]
+node-2 ansible_host=10.20.0.3
+node-5 ansible_host=10.20.0.6
+
+[compute]
+node-1 ansible_host=10.20.0.2
+node-2 ansible_host=10.20.0.3
+
+[controller]
+node-3 ansible_host=10.20.0.4
+node-4 ansible_host=10.20.0.5
+node-5 ansible_host=10.20.0.6
+
+[mongo]
+node-4 ansible_host=10.20.0.5
+
+
+'''
+
+        args = 'node list --ansible-inventory'
+        with mock.patch('sys.stdout', new=cStringIO.StringIO()) as mstdout:
+            rv = self.exec_command(args)
+            actual_output = mstdout.getvalue()
+
+        self.assertFalse(rv)
+        self.assertEqual(expected_output, actual_output)
+
+        self.m_get_client.assert_called_once_with('node', mock.ANY)
+        self.m_client.get_all.assert_called_once_with(
+            environment_id=None, labels=None)
 
     def test_node_show(self):
         node_id = 42
