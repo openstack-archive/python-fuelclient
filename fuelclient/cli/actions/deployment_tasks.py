6847874bb8ee2e4cb80a39b0799a9ce6ec5182c5
Vladimir Sharshov (warpc) <vsharshov@mirantis.com>
2016-03-18 21:45:21 +0300

    Support deployment history
    Change-Id: Ia999b55dbe302a08ae68a6afe12f3ef2b568b547
    Implements: blueprint store-deployment-tasks-history

---
+++ fuelclient/cli/actions/deployment_tasks.py
@@ -0,0 +1,72 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelclient.cli.actions.base import Action
+import fuelclient.cli.arguments as Args
+from fuelclient.cli.arguments import group
+from fuelclient.cli.formatting import format_table
+from fuelclient.objects.deployment_history import DeploymentHistory
+
+
+class DeploymentTasksAction(Action):
+    """Show deployment tasks
+    """
+    action_name = "deployment-tasks"
+
+    def __init__(self):
+        super(DeploymentTasksAction, self).__init__()
+        self.args = [
+            group(
+                Args.get_list_arg("List all deployment tasks"),
+            ),
+            Args.get_single_task_arg(required=True),
+            Args.get_deployment_node_arg(
+                "Node ids."
+            ),
+            Args.get_status_arg(
+                "Statuses: pending, error, ready, running"
+            )
+        ]
+        self.flag_func_map = (
+            (None, self.list),
+        )
+
+    def list(self, params):
+        """To display all deployment tasks:
+                fuel deployment-tasks --task-id 5
+
+            To display deployment tasks with for some nodes:
+                fuel deployment-tasks --task-id 5 --nodes 1,2
+
+            To display deployment tasks with for some statuses(pending, error,
+            ready, running):
+                fuel deployment-tasks --task-id 5 --statuses pending,running
+
+            To display deployment tasks with for some statuses(pending, error,
+            ready, running) on some nodes:
+                fuel deployment-tasks --task-id 5 --statuses error --nodes 1,2
+        """
+
+        acceptable_keys = ("deployment_graph_task_name", "node_id", "status",
+                           "time_start", "time_end")
+
+        d_tasks_data = DeploymentHistory.get_all_with_nodes_and_statuses(
+            params.task,
+            params.node,
+            params.status
+        )
+        self.serializer.print_to_output(
+            d_tasks_data,
+            format_table(d_tasks_data, acceptable_keys=acceptable_keys)
+        )
