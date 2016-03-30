9f4fb2532222b752d2e37222336c6fdf2b1c9377
Ilya Kutukov <ikutukov@mirantis.com>
2016-03-30 14:18:18 +0300

    Add custom graph support to the Fuel V2 CLI
    Following commands are added:
    
    * fuel2 graph list --env env_id
    
    * fuel2 graph upload --env env_id [--type graph_type] --file tasks.yaml
    
    * fuel2 graph upload --release release_id [--type graph_type] --file tasks.yaml
    
    * fuel2 graph upload --plugin plugin_id [--type graph_type] --file tasks.yaml
    
    --type is optional. ‘default’ graph type with confirmation should be used if no type is defined.
    
    * fuel2 graph download --env env_id --all [--type graph_type] [--file cluster_graph.yaml]
    
    * fuel2 graph download --env env_id --cluster [--type graph_type] [--file cluster_graph.yaml]
    
    * fuel2 graph download --env env_id --plugins [--type graph_type] [--file plugins_graph.yaml]
    
    * fuel2 graph download --env env_id --release [--type graph_type] [--file release_graph.yaml]
    
    --type is optional and ‘default’ graph will be downloaded in no type is
    defined.
    
    * fuel2 graph execute --env env_id [--type graph_type] [--node node_ids] Graph execution available only for the environment.
    
    Change-Id: I33b168b7929b10200709efc58fd550f8779b39ae
    Closes-Bug: #1563851

---
+++ fuelclient/tests/unit/v2/cli/test_deployment_graph.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from six import moves
+
+from fuelclient.tests.unit.v2.cli import test_engine
+
+
+class TestGraphActions(test_engine.BaseCLITest):
+
+    def _test_cmd(self, method, cmd_line, expected_kwargs):
+        self.m_get_client.reset_mock()
+        self.m_client.get_filtered.reset_mock()
+        self.m_client.__getattr__(method).return_value = 'graph.yaml'
+        self.exec_command('graph {0} {1}'.format(method, cmd_line))
+        self.m_get_client.assert_called_once_with('graph', mock.ANY)
+        self.m_client.__getattr__(method).assert_called_once_with(
+            **expected_kwargs)
+
+    def test_upload(self):
+        self._test_cmd('upload', '--env 1 --file new_graph.yaml', dict(
+            graph_type=None,
+            file_path='new_graph.yaml',
+            cluster_id=1,
+            release_id=None,
+            plugin_id=None,
+        ))
+        self._test_cmd('upload', '--release 1 --file new_graph.yaml', dict(
+            graph_type=None,
+            file_path='new_graph.yaml',
+            cluster_id=None,
+            release_id=1,
+            plugin_id=None,
+        ))
+        self._test_cmd('upload', '--plugin 1 --file new_graph.yaml', dict(
+            graph_type=None,
+            file_path='new_graph.yaml',
+            cluster_id=None,
+            release_id=None,
+            plugin_id=1,
+        ))
+        self._test_cmd('upload', '--file new_graph.yaml', dict(
+            graph_type=None,
+            file_path='new_graph.yaml',
+            cluster_id=None,
+            release_id=None,
+            plugin_id=None,
+        ))
+
+    def test_download(self):
+        self._test_cmd(
+            'download',
+            '--env 1 --all --file existing_graph.yaml --type custom_graph',
+            dict(
+                env_id=1,
+                all=True,
+                cluster=False,
+                plugins=False,
+                release=False,
+                file_path='existing_graph.yaml',
+                graph_type='custom_graph'
+            )
+        )
+
+    def test_execute(self):
+        self._test_cmd(
+            'execute',
+            '--env 1 --type custom_graph --nodes 1 2 3',
+            dict(
+                env_id=1,
+                graph_type='custom_graph',
+                nodes=[1, 2, 3]
+            )
+        )
+
+    def test_list(self):
+        with mock.patch('sys.stdout', new=moves.cStringIO()) as m_stdout:
+            self.m_get_client.reset_mock()
+            self.m_client.get_filtered.reset_mock()
+            self.m_client.list.return_value = [
+                {
+                    'name': 'updated-graph-name',
+                    'tasks': [{
+                        'id': 'test-task2',
+                        'type': 'puppet',
+                        'task_name': 'test-task2',
+                        'version': '2.0.0'
+                    }],
+                    'relations': [{
+                        'model': 'cluster',
+                        'model_id': 370,
+                        'type': 'custom-graph'
+                    }],
+                    'id': 1
+                }
+            ]
+            self.exec_command('graph list --env 1')
+            self.m_get_client.assert_called_once_with('graph', mock.ANY)
+            self.m_client.list.assert_called_once_with(env_id=1)
+
+            self.assertIn('1', m_stdout.getvalue())
+            self.assertIn('updated-graph-name', m_stdout.getvalue())
+            self.assertIn('custom-graph', m_stdout.getvalue())
+            self.assertIn('test-task2', m_stdout.getvalue())
