3d368b33c4914bd0df78152033267cc4b7f1bdf5
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-04-08 12:34:11 +0300

    Add notice about updating fuel_client.yaml when updating pass
    Users are advised to update their configuration after
    changing their Fuel Admin password.
    
    Change-Id: Ie4f7e29092002cb316829ef394e4c938ebf30766
    Partial-Bug: #1555262

--- fuelclient/tests/unit/v1/test_user_action.py
+++ fuelclient/tests/unit/v1/test_user_action.py
@@ -16,10 +16,14 @@ import mock
 
 from fuelclient.cli.actions.user import UserAction
 from fuelclient.cli.error import ArgumentException
+from fuelclient.cli.serializers import Serializer
 from fuelclient.tests.unit.v1 import base
 
 
 class TestChangePassword(base.UnitTestCase):
+
+    def assert_print(self, print_mock, result, msg):
+        print_mock.assert_called_once_with(result, msg)
 
     def test_get_password_from_prompt(self):
         user_action = UserAction()
@@ -37,9 +41,23 @@ class TestChangePassword(base.UnitTestCase):
         with self.assertRaisesRegexp(
                 ArgumentException, 'Passwords are not the same'):
             user_action._get_password_from_prompt()
+#
+    @patch.object(Serializer, 'print_to_output')
+    @patch.object(PluginAction, 'check_file')
+    @patch.object(Plugins, 'install', return_value='some_result')
+    def test_install(self, install_mock, check_mock, print_mock):
+        self.exec_plugins(['--install', self.file_name])
+        self.assert_print(
+            print_mock,
+            'some_result',
+            'Plugin /tmp/path/plugin.fp was successfully installed.')
+        install_mock.assert_called_once_with(self.file_name, force=False)
+        check_mock.assert_called_once_with(self.file_name)
+#
 
+    @patch.object(Serializer, 'print_to_output')
     @mock.patch('fuelclient.cli.actions.user.APIClient')
-    def test_change_password(self, mapiclient):
+    def test_change_password(self, print_mock, mapiclient):
         user_action = UserAction()
         params = mock.Mock()
         params.newpass = None
@@ -56,6 +74,14 @@ class TestChangePassword(base.UnitTestCase):
 
         mgetpass.assert_has_calls(calls)
         mapiclient.update_own_password.assert_called_once_with(password)
+        msg = "\nPassword changed.\nPlease note that configuration " \
+            "is not automatically updated.\nYou may want to update " \
+            "/root/.config/fuel/fuel_client.yaml."
+
+        self.assert_print(
+            print_mock,
+            None,
+            msg)
 
     @mock.patch('fuelclient.cli.actions.user.APIClient')
     def test_change_password_w_newpass(self, mapiclient):
