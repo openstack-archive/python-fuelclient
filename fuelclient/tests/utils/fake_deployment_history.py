8a276ed9d3d875c9ca467797bf93111b7a65f2bb
Ilya Kutukov <ikutukov@mirantis.com>
2016-05-16 20:28:35 +0300

    Transaction tasks parameters support is added to CLI
    CLI parameter added
    
    fuel deployment-tasks --tid 1 --task-name mytask1,mytask2
    fuel2 task history show 1 --tasks-names mytask1 mytask2
    
    Deployment tasks history is now grouped by tasks.
    
    Closes-Bug: #1563317
    DocImpact
    
    Change-Id: I495b8884bea502b0eb9c10024e72745c688c3f58
    (cherry picked from commit 058983896f953aec1bef481f5407da9d4f212293)

--- fuelclient/tests/utils/fake_deployment_history.py
+++ fuelclient/tests/utils/fake_deployment_history.py
@@ -13,28 +13,101 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import itertools
 
 
-def get_fake_deployment_history():
-    """Create a fake deployment history
+def get_fake_deployment_history(
+        add_task_data=False, convert_legacy_fields=False):
+    """Create a fake deployment history.
 
     Returns the serialized and parametrized representation of a dumped Fuel
     Deployment History. Represents the average amount of data.
 
+    :param add_task_data: add task description to history records using
+                          Fuel 10.0 history output format
+    :type add_task_data: bool
+    :param convert_legacy_fields: Fuel 9.0 output fields are renamed to 10.0
+                                  if True
+    :type convert_legacy_fields: True
+    :returns: fake deployment fixtures
+    :rtype: list[dict]
     """
-    return [
-        {
-            "status": "ready",
-            "time_start": "2016-03-25T17:22:10.687135",
-            "time_end": "2016-03-25T17:22:30.830701",
-            "node_id": "1",
-            "deployment_graph_task_name": "controller_remaining_tasks"
-        },
-        {
-            "status": "skipped",
-            "time_start": "2016-03-25T17:23:37.313212",
-            "time_end": "2016-03-25T17:23:37.313234",
-            "node_id": "2",
-            "deployment_graph_task_name": "ironic-compute"
-        }
-    ]
+    if add_task_data:
+        return list(itertools.chain(*[[
+            {
+                'status': 'ready',
+                'time_start': '2016-03-25T17:22:10.687135',
+                'time_end': '2016-03-25T17:22:30.830701',
+                'node_id': node_id,
+                'task_name': 'controller-remaining-tasks',
+                'type': 'puppet',
+                'role': ['controller'],
+                'version': '2.0.0',
+                'parameters': {
+                    'puppet_manifest': '/etc/puppet/modules/osnailyfacter'
+                                       '/modular/globals/globals.pp',
+                    'puppet_modules': '/etc/puppet/modules',
+                    'timeout': 3600
+                }
+            },
+            {
+                'status': 'skipped',
+                'time_start': '2016-03-25T17:23:37.313212',
+                'time_end': '2016-03-25T17:23:37.313234',
+                'node_id': node_id,
+                'task_name': 'ironic-compute',
+                'type': 'puppet',
+                'role': ['controller'],
+                'version': '2.0.0',
+                'parameters': {
+                    'puppet_manifest': '/etc/puppet/modules/osnailyfacter'
+                                       '/modular/globals/globals.pp',
+                    'puppet_modules': '/etc/puppet/modules',
+                    'timeout': 3600
+                }
+            },
+            {
+                'status': 'pending',
+                'time_start': None,
+                'node_id': node_id,
+                'task_name': 'pending-task',
+                'type': 'puppet',
+                'role': ['controller'],
+                'version': '2.0.0',
+                'parameters': {
+                    'puppet_manifest': '/etc/puppet/modules/osnailyfacter'
+                                       '/modular/globals/globals.pp',
+                    'puppet_modules': '/etc/puppet/modules',
+                    'timeout': 3600
+                }
+            }
+        ] for node_id in ['1', '2']]))
+    else:
+        result = list(itertools.chain(*[[
+            {
+                'status': 'ready',
+                'time_start': '2016-03-25T17:22:10.687135',
+                'time_end': '2016-03-25T17:22:30.830701',
+                'node_id': node_id,
+                'deployment_graph_task_name': 'controller-remaining-tasks'
+            },
+            {
+                'status': 'skipped',
+                'time_start': '2016-03-25T17:23:37.313212',
+                'time_end': '2016-03-25T17:23:37.313234',
+                'node_id': node_id,
+                'deployment_graph_task_name': 'ironic-compute'
+            },
+            {
+                'status': 'pending',
+                'time_start': None,
+                'node_id': node_id,
+                'deployment_graph_task_name': 'pending-task'
+            }
+        ] for node_id in ['1', '2']]))
+
+        if convert_legacy_fields:
+            for record in result:
+                record['task_name'] = record['deployment_graph_task_name']
+                record.pop('deployment_graph_task_name', None)
+        return result
