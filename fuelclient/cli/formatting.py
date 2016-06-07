25117f1682e8cc095c35ab019dbaa1c7172cda63
Mikhail <mzhnichkov@mirantis.com>
2016-06-07 14:49:11 +0300

    Fix fuel cli output function
    FUEL CLI output function may request progress for deleted node.
    This patch introduces check for node's existence.
    
    Change-Id: Ia85db174976cb0dbacaf20e6378fcbb9f1ed7cf0
    Closes-Bug: #1565026

--- fuelclient/cli/formatting.py
+++ fuelclient/cli/formatting.py
@@ -194,11 +194,23 @@ def print_deploy_progress_with_terminal(deploy_task, terminal_screen):
                 )
             )
             for index, node in enumerate(nodes):
-                terminal_screen.addstr(
-                    index + 2, 0,
-                    "Node{id:3} {status:13}: {bar} {progress:3}%"
-                    .format(bar=node_bar(node.progress), **node.data)
-                )
+                data = {
+                    'id': node.id,
+                    'status': 'removed',
+                    'progress': 100
+                }
+                if node.exists():
+                    terminal_screen.addstr(
+                        index + 2, 0,
+                        "Node{id:3} {status:13}: {bar} {progress:3}%"
+                        .format(bar=node_bar(node.progress), **node.data)
+                    )
+                else:
+                    terminal_screen.addstr(
+                        index + 2, 0,
+                        "Node{id:3} {status:13}: {bar} {progress:3}%"
+                        .format(bar=node_bar(data['progress']), **data)
+                    )
     except DeployProgressError as de:
         close_curses()
         print(de.message)
