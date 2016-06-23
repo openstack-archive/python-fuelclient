90ac178bd22c2154158c8cd49972fde83ae30da1
Vitalii Myhal <vmygal@mirantis.com>
2016-06-23 13:44:35 +0300

    Plugin management code was moved from fuelclient to nailgun.
    Nailgun now has its own plugins packet manager.
    
    Added new API endpoint:
      /plugins/upload
    
    Added new commands:
      fuel2 plugins install
      fuel2 plugins remove
    
    Removed old command:
      fuel plugins
    
    Partial-Bug: 1504338
    Partial-Bug: 1562466 (https://bugs.launchpad.net/fuel/+bug/1562466)
    Change-Id: I88810a30f9630de14b9ed5eb56e425121a555f6b
    Depends-On: I599714beacd6aaf005788b63376acdcb729ec888

--- fuelclient/v1/plugins.py
+++ fuelclient/v1/plugins.py
@@ -13,15 +13,53 @@
 #    under the License.
 
 import collections
+import os
+
 import six
 
-from fuelclient import objects
+from fuelclient.cli import error
 from fuelclient.v1 import base_v1
 
 
-class PluginsClient(base_v1.BaseV1Client):
+class PluginBase(object):
 
-    _entity_wrapper = objects.Plugins
+    @staticmethod
+    def pre_install(path='/', ext=''):
+        raise error.BadDataException(
+            "Plugin '{0}' has unsupported format '{1}'.".format(
+                os.path.basename(path), ext))
+
+
+class PluginFP(object):
+
+    msg_deprecation = "The plugin has old 1.0 package format. " \
+        "This format does not support many features, such as updating " \
+        "plugins. Find plugin in new format or migrate and rebuild this one.\n"
+
+    def pre_install(self, *args, **kwargs):
+        if kwargs.get('ext') == 'fp':
+            error.print_deprecation_warning(self.msg_deprecation)
+            return
+        super(PluginFP, self).pre_install(*args, **kwargs)
+
+
+class PluginRPM(object):
+
+    def pre_install(self, *args, **kwargs):
+        if kwargs.get('ext') == 'rpm':
+            return
+        super(PluginRPM, self).pre_install(*args, **kwargs)
+
+
+class PluginsClient(PluginRPM, PluginFP, PluginBase, base_v1.BaseV1Client):
+
+    class_api_path = 'plugins'
+    instance_api_path = 'plugins/{id}'
+    install_api_path = 'plugins/upload'
+    sync_api_path = 'plugins/sync'
+
+    def _get_all_data(self):
+        return self.connection.get_request(self.class_api_path)
 
     def get_all(self):
         """Get plugins data and re-format 'releases' info to display
@@ -35,7 +73,7 @@ class PluginsClient(base_v1.BaseV1Client):
         # Replace original nested 'releases' dictionary (from plugins meta
         # dictionary) to a new user-friendly form with releases info, i.e.
         # 'os', 'version' that specific plugin supports
-        plugins = self._entity_wrapper.get_all_data()
+        plugins = self._get_all_data()
         for plugin in plugins:
             releases = collections.defaultdict(list)
             for key in plugin['releases']:
@@ -44,14 +82,55 @@ class PluginsClient(base_v1.BaseV1Client):
                                            for k, v in six.iteritems(releases))
         return plugins
 
-    def sync(self, ids):
+    def install(self, path, force=False):
+        """Installs a plugin package and updates data in API service.
+
+        :param path: Path to plugin file
+        :type path: str
+        :param force: Updates existent plugin even if it is not updatable
+        :type force: bool
+        :return: Plugin information
+        :rtype: dict
+        """
+        self.pre_install(path=path, ext=os.path.splitext(path)[1][1:].lower())
+
+        return self.connection.post_upload_file_raw(
+            self.install_api_path, path, data={'force': force})
+
+    def remove(self, name, version):
+        """Removes a plugin package and updates data in API service.
+
+        :param name: Plugin name
+        :type name: str
+        :param version: Plugin version
+        :type version: str
+        :return: Result of deleting
+        :rtype: dict
+        :raise: error.BadDataException if no plugin was found
+        """
+        plugins = [p for p in self._get_all_data()
+                   if (p['name'], p['version']) == (name, version)]
+        if not plugins:
+            raise error.BadDataException(
+                "Plugin '{0}' with version '{1}' does not exist.".format(
+                    name, version))
+
+        return self.connection.delete_request(
+            self.instance_api_path.format(**plugins[0]))
+
+    def sync(self, ids=None):
         """Synchronise plugins on file system with plugins in API service.
 
-        :param ids: List of ids for filtering plugins
+        :param ids: List of plugins IDs which should be synced
         :type ids: list
+        :return: Result of syncing
+        :rtype: dict
         """
+        data = None
+        if ids is not None:
+            data = {'ids': ids}
 
-        self._entity_wrapper.sync(plugin_ids=ids)
+        return self.connection.post_request(self.sync_api_path, data=data)
 
 
 def get_client(connection):
