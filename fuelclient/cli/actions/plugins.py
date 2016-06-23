dc66e0bcfc9ddab8bdb52ab811e67d7766cc8bab
Vitalii Myhal <vmygal@mirantis.com>
2016-06-23 13:44:35 +0300

    Plugin management code has moved to nailgun from fuelclient.
    Nailgun now has its own plugins packet manager.
    
    Added new API endpoint:
      /plugins/upload
    
    Added new commands:
      fuel2 plugins install
      fuel2 plugins remove
    
    Change-Id: I88810a30f9630de14b9ed5eb56e425121a555f6b
    Partial-Bug: 1504338
    Partial-Bug: 1562466 (https://bugs.launchpad.net/fuel/+bug/1562466)
    Depends-On: I599714beacd6aaf005788b63376acdcb729ec888

--- fuelclient/cli/actions/plugins.py
+++ fuelclient/cli/actions/plugins.py
@@ -13,6 +13,7 @@
 #    under the License.
 
 import collections
+import os
 import six
 
 import fuelclient.cli.arguments as Args
@@ -44,17 +45,9 @@ class PluginAction(Action):
                 Args.get_list_arg(
                     "List of all registered plugins."),
                 Args.get_plugin_install_arg(
-                    "Install and register plugin package"),
+                    "Install plugin package"),
                 Args.get_plugin_remove_arg(
-                    "Remove and unregister plugin"),
-                Args.get_plugin_register_arg(
-                    "Register installed plugin"),
-                Args.get_plugin_unregister_arg(
-                    "Unregister plugin"),
-                Args.get_plugin_update_arg(
-                    "Update installed plugin"),
-                Args.get_plugin_downgrade_arg(
-                    "Downgrade installed plugin"),
+                    "Remove plugin"),
                 Args.get_plugin_sync_arg(
                     "Synchronise plugins with API service")),
             Args.get_plugin_arg("Plugin id."),
@@ -63,21 +56,17 @@ class PluginAction(Action):
         self.flag_func_map = (
             ("install", self.install),
             ("remove", self.remove),
-            ("update", self.update),
-            ("downgrade", self.downgrade),
             ("sync", self.sync),
-            ("register", self.register),
-            ("unregister", self.unregister),
             (None, self.list),
         )
 
     def list(self, params):
         """Print all available plugins
 
-                fuel plugins
-                fuel plugins --list
+              fuel plugins
+              fuel plugins --list
         """
-        plugins = Plugins.get_all_data()
+        plugins = Plugins().get_all_data()
         # Replace original nested 'release' dictionary (from plugins meta
         # dictionary) to flat one with necessary release info (os, version)
         for plugin in plugins:
@@ -91,101 +80,59 @@ class PluginAction(Action):
             format_table(plugins, acceptable_keys=self.acceptable_keys))
 
     def install(self, params):
-        """Install plugin archive and register in API service
+        """Install, upgrade or downgrade plugin and register it in API service.
+            Upgrade or downgrade plugin is allowed from one minor version
+            to another:
+            2.0.0 -> 2.0.1 - allowed
+            2.0.0 -> 2.1.0 - cannot be used to perform upgrade,
+                a new version of a plugin will be installed
+            2.0.1 -> 2.0.0 - allowed
+            2.0.1 -> 1.0.0 - cannot be used to perform downgrade,
+                a new version of a plugin will be installed
 
-               fuel plugins --install plugin-name-2.0-2.0.1-0.noarch.rpm
+              fuel plugins --install plugin-name-2.0-2.0.1-0.noarch.rpm
+
+            Note: upgrade and downgrade is supported for plugins beginning
+            from package_version 2.0.0
+
+            To reinstall plugin with the same version is used '--force' flag:
+
+              fuel plugins --install plugin-name-2.0-2.0.1-0.noarch.rpm --force
+
         """
         file_path = params.install
         self.check_file(file_path)
-        results = Plugins.install(file_path, force=params.force)
+        result = Plugins().install(file_path, force=params.force)
         self.serializer.print_to_output(
-            results,
-            "Plugin {0} was successfully installed.".format(
-                params.install))
+            result,
+            "Plugin '{0}' was successfully {1}.".format(
+                os.path.basename(file_path), result['action']))
 
     def remove(self, params):
-        """Remove plugin from file system and from API service
+        """Remove plugin from file system and from API service.
 
-               fuel plugins --remove plugin-name==1.0.1
+              fuel plugins --remove plugin-name==1.0.1
         """
         name, version = self.parse_name_version(params.remove)
-        results = Plugins.remove(name, version)
-
-        self.serializer.print_to_output(
-            results,
-            "Plugin {0} was successfully removed.".format(params.remove))
-
-    def update(self, params):
-        """Update plugin from one minor version to another.
-           For example if there is a plugin with version 2.0.0,
-           plugin with version 2.0.1 can be used as update. But
-           plugin with version 2.1.0, cannot be used to update
-           plugin. Note that update is supported for plugins
-           beginning from package_version 2.0.0
-
-               fuel plugins --update plugin-name-2.0-2.0.1-0.noarch.rpm
-        """
-        plugin_path = params.update
-        self.check_file(plugin_path)
-        result = Plugins.update(plugin_path)
+        result = Plugins().remove(name, version)
         self.serializer.print_to_output(
             result,
-            "Plugin {0} was successfully updated.".format(plugin_path))
-
-    def downgrade(self, params):
-        """Downgrade plugin from one minor version to another.
-           For example if there is a plugin with version 2.0.1,
-           plugin with version 2.0.0 can be used to perform downgrade.
-           Plugin with version 1.0.0, cannot be used to perform downgrade
-           plugin. Note that downgrade is supported for plugins
-           beginning from package_version 2.0.0
-
-               fuel plugins --downgrade plugin-name-2.0-2.0.1-0.noarch.rpm
-        """
-        plugin_path = params.downgrade
-        self.check_file(plugin_path)
-        result = Plugins.downgrade(plugin_path)
-        self.serializer.print_to_output(
-            result,
-            "Plugin {0} was successfully downgraded.".format(plugin_path))
+            "Plugin '{0}' was successfully removed.".format(params.remove))
 
     def sync(self, params):
-        """Synchronise plugins on file system with plugins in
-           API service, creates plugin if it is not exists,
-           updates existent plugins
+        """Synchronise plugins on file system with plugins in API service,
+            creates plugin if it is not exists, updates existent plugins.
 
-               fuel plugins --sync
-               fuel plugins --sync --plugin-id=1,2
+              fuel plugins --sync
+              fuel plugins --sync --plugin-id=1,2
         """
-        Plugins.sync(plugin_ids=params.plugin)
+        Plugins().sync(plugin_ids=params.plugin)
         self.serializer.print_to_output(
             None, "Plugins were successfully synchronized.")
 
-    def register(self, params):
-        """Register plugin in API service
-
-               fuel plugins --register plugin-name==1.0.1
-        """
-        name, version = self.parse_name_version(params.register)
-        result = Plugins.register(name, version, force=params.force)
-        self.serializer.print_to_output(
-            result,
-            "Plugin {0} was successfully registered.".format(params.register))
-
-    def unregister(self, params):
-        """Deletes plugin from API service
-
-               fuel plugins --unregister plugin-name==1.0.1
-        """
-        name, version = self.parse_name_version(params.unregister)
-        result = Plugins.unregister(name, version)
-        self.serializer.print_to_output(
-            result,
-            "Plugin {0} was successfully unregistered."
-            "".format(params.unregister))
-
-    def parse_name_version(self, param):
-        """Takes the string and returns name and version
+    @staticmethod
+    def parse_name_version(param):
+        """Takes the string and returns name and version.
 
         :param str param: string with name and version
         :raises: error.ArgumentException if version is not specified
@@ -198,8 +145,9 @@ class PluginAction(Action):
 
         return attrs
 
-    def check_file(self, file_path):
-        """Checks if file exists
+    @staticmethod
+    def check_file(file_path):
+        """Checks if file exists.
 
         :param str file_path: path to the file
         :raises: error.ArgumentException if file does not exist
