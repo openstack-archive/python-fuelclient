d33843f850d06f3f5ae195dfe1d4dda80495fc0f
tivaliy <vitaliy@kulanov.org.ua>
2016-07-09 21:05:13 +0300

    Add 'fuel2 release component list' command
    In Nailgun there is an API call for getting JSON data
    of compatible components for release and all plugins
    releated with it (GET /api/v1/releases/<:id>/components/).
    This patch adds new fuel2 command, e.g.:
    
           fuel2 release component list 1
    
    This command prints list of components for given Fuel release
    with respective cross-dependencies
    
    DocImpact
    Closes-Bug: 1600501
    
    Change-Id: I055d6fb4dde4cb0ac9508084155bbce5a106220c

--- fuelclient/tests/utils/fake_release.py
+++ fuelclient/tests/utils/fake_release.py
@@ -60,3 +60,48 @@ def get_fake_release(release_id=None, name=None, state=None,
         'version': version or 'mitaka-9.0',
         'attributes_metadata': get_fake_attributes_metadata(repos=repos),
     }
+
+
+def get_fake_release_component(name=None, requires=None, incompatible=None,
+                               compatible=None, default=None):
+    """Create a random fake component of release
+
+    Returns the serialized and parametrized representation of a dumped Fuel
+    component of release. Represents the average amount of data.
+
+    """
+    return {
+        'name': name or 'network:neutron:ml2:vlan',
+        'description':
+            'dialog.create_cluster_wizard.network.neutron_vlan_description',
+        'weight': 5,
+        'requires': requires or [
+            {'name': 'network:neutron:core:ml2'}
+        ],
+        'incompatible': incompatible or [
+            {'message': 'dialog.create_cluster_wizard.network.vlan_tun_alert',
+             'name': 'network:neutron:ml2:tun'}
+        ],
+        'compatible': compatible or [
+            {'name': 'network:neutron:core:ml2'},
+            {'name': 'hypervisor:qemu'},
+            {'name': 'hypervisor:vmware'},
+            {'name': 'storage:block:lvm'},
+            {'name': 'storage:block:ceph'},
+            {'name': 'storage:object:ceph'},
+            {'name': 'storage:ephemeral:ceph'},
+            {'name': 'storage:image:ceph'},
+            {'name': 'additional_service:sahara'},
+            {'name': 'additional_service:murano'},
+            {'name': 'additional_service:ceilometer'},
+            {'name': 'additional_service:ironic'}
+        ],
+        'default': default or True,
+        'label': 'common.network.neutron_vlan',
+    }
+
+
+def get_fake_release_components(component_count, **kwargs):
+    """Create a random fake list of release components."""
+    return [get_fake_release(release_id=i, **kwargs)
+            for i in range(1, component_count + 1)]
