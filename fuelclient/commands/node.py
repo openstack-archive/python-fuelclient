fc50379d0b6f5ae44317b8589db08b9252dff1bf
Roman Prykhodchenko <me@romcheg.me>
2016-07-12 19:49:56 +0200

    Add common code for node attributes
    In order to facilitace adding different CLI commands for
    managing different attributes of nodes there is a need to
    add some common code that will allow to store and load
    attributes along with the code that allow to find proper
    file path to them.
    
    (cherry-picked from 94ba15e)
    
    Change-Id: I82e61de64f75a582f2ca49bfde08fda041586f04

--- fuelclient/commands/node.py
+++ fuelclient/commands/node.py
@@ -28,6 +28,45 @@ from fuelclient import utils
 class NodeMixIn(object):
     entity_name = 'node'
 
+    numa_fields = (
+        'numa_nodes',
+        'supported_hugepages',
+        'distances')
+
+    supported_file_formats = ('json', 'yaml')
+    allowed_attr_types = ('attributes', 'disks', 'interfaces')
+
+    @classmethod
+    def get_numa_topology_info(cls, data):
+        numa_topology_info = {}
+        numa_topology = data['meta'].get('numa_topology', {})
+        for key in cls.numa_fields:
+            numa_topology_info[key] = numa_topology.get(key)
+        return numa_topology_info
+
+    def get_attributes_path(self, attr_type, file_format, node_id, directory):
+        """Returnes a path for attributes of a node
+
+        :param attr_type:   Attribute type.
+                            Should be one of {attributes, disks, interfaces}
+        :param file_format: The format of the file that contains or will
+                            contain the attributes. Must be json or yaml.
+        :param node_id:     Id of a node
+        :param directory:   Directory that is used to store attributes.
+
+        """
+        if attr_type not in self.allowed_attr_types:
+            raise ValueError('attr_type must be '
+                             'one of {}'.format(self.allowed_attr_types))
+
+        if file_format not in self.supported_file_formats:
+            raise ValueError('file_format must be '
+                             'one of {}'.format(self.supported_file_formats))
+
+        return os.path.join(os.path.abspath(directory),
+                            'node_{0}'.format(node_id),
+                            '{}.{}'.format(attr_type, file_format))
+
 
 class NodeList(NodeMixIn, base.BaseListCommand):
     """Show list of all available nodes."""
