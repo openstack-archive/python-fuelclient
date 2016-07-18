671bfd0494da0103c970e41940941edb4e5a4b8e
Roman Prykhodchenko <me@romcheg.me>
2016-07-18 17:50:40 +0200

    Add fuel2 commands for node disks
    This patch adds commands that allow to download and upload
    configuration of disks for a node:
    
     - fuel2 node disk upload
     - fuel2 node disk download
     - fuel2 node disk get-default
    
    DocImpact
    Change-Id: I4a3c01c6ea3f0afb1c994cf4937f1a429c5314f9

--- fuelclient/v1/node.py
+++ fuelclient/v1/node.py
@@ -168,6 +168,37 @@ class NodeClient(base_v1.BaseV1Client):
         return node.write_attribute(
             'attributes', attributes, directory=directory)
 
+    def get_disks(self, node_id):
+        """Download configuration of diskss for a node
+
+        :param node_id: Id of a node.
+        :return:        dict with the configuration of disks for the node.
+
+        """
+        node = self._entity_wrapper(node_id)
+        return node.get_attribute('disks')
+
+    def get_default_disks(self, node_id):
+        """Download default configuration of disks for a node
+
+        :param node_id: Id of a node.
+        :return:        dict with the configuration of disks for the node.
+
+        """
+        node = self._entity_wrapper(node_id)
+        return node.get_default_attribute('disks')
+
+    def set_disks(self, node_id, disks):
+        """Upload and set configuration of disks for a node
+
+        :param node_id:    Id of a node.
+        :param interfaces: dict that contains valid configuration
+                           for disks
+
+        """
+        node = self._entity_wrapper(node_id)
+        return node.upload_node_attribute('disks', disks)
+
     def upload_attributes(self, node_id, directory=None):
         node = self._entity_wrapper(node_id)
         attributes = node.read_attribute('attributes', directory=directory)
