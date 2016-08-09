e41e6511a9963f607c20aae2c12e1c04d1440c70
Vladimir Kuklin <vkuklin@mirantis.com>
2016-08-09 19:43:20 +0300

    Add subgraph execution support
    Add -S option to support subgraph execution. This would allow a user
    to pick which tasks to stop on.
    
    Example:
    
    fuel2 graph execute -S keystone-db:openstack-controller/1
    
    will start with all keystone-db tasks and end at openstack-controller at
    node-1
    
    DocImpact
    
    Change-Id: I42f9caf2e9c18072a2ae0447df2ba3500f687d5e
    Closes-bug: 1612616

--- fuelclient/commands/graph.py
+++ fuelclient/commands/graph.py
@@ -186,6 +186,15 @@ class GraphExecute(base.BaseTasksExecuteCommand):
             nargs='+',
             help='List of deployment tasks to run.'
         )
+        parser.add_argument('-S',
+                            '--subgraphs',
+                            type=str,
+                            nargs='+',
+                            required=False,
+                            help='List of subgraphs to execute'
+                                 'Format is: '
+                                 '[<start_task>[/<node_ids>]]\
+                                 [:<end_task>/[<node_ids>]]')
         return parser
 
     def get_options(self, parsed_args):
@@ -193,7 +202,7 @@ class GraphExecute(base.BaseTasksExecuteCommand):
             'graph_types': parsed_args.graph_types,
             'nodes': parsed_args.nodes,
             'task_names': parsed_args.task_names,
-
+            'subgraphs': parsed_args.subgraphs
         }
 
 
