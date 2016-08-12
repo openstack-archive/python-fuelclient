eed726efa5e0ec687ebe918b8e54657aee39f63d
tivaliy <vitaliy@kulanov.org.ua>
2016-08-12 08:20:29 +0300

    Add fuel2 diagnostic snapshot commands
    This patch adds commands that allow to manipulate
    with diagnostic snapshot process creation:
    
       fuel2 task snapshot get-default-config
       fuel2 task snapshot create
    
    DocImpact
    
    Change-Id: I59542eba922d793406c50b864b1cd0c416c7c891

--- fuelclient/tests/unit/v2/cli/test_task.py
+++ fuelclient/tests/unit/v2/cli/test_task.py
@@ -14,6 +14,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import json
 import mock
 import os
 import yaml
@@ -144,6 +145,78 @@ class TestTaskCommand(test_engine.BaseCLITest):
                        "{0}/network_configuration_1.yaml".format(
                            self.current_path),
                        dict(transaction_id=1))
+
+    @mock.patch('json.dump')
+    def test_task_snapshot_config_download_json(self, m_dump):
+        args = 'task snapshot get-default-config -f json -d /tmp'
+        test_data = {'foo': 'bar'}
+        expected_path = '/tmp/snapshot_conf.json'
+
+        self.m_client.get_default_config.return_value = test_data
+
+        m_open = mock.mock_open()
+        with mock.patch('fuelclient.commands.task.open', m_open, create=True):
+            self.exec_command(args)
+
+        m_open.assert_called_once_with(expected_path, 'w')
+        m_dump.assert_called_once_with(test_data, mock.ANY, indent=4)
+        self.m_get_client.assert_called_once_with('snapshot', mock.ANY)
+        self.m_client.get_default_config.assert_called_once_with()
+
+    @mock.patch('yaml.safe_dump')
+    def test_task_snapshot_config_download_yaml(self, m_safe_dump):
+        args = 'task snapshot get-default-config -f yaml -d /tmp'
+        test_data = {'foo': 'bar'}
+        expected_path = '/tmp/snapshot_conf.yaml'
+
+        self.m_client.get_default_config.return_value = test_data
+
+        m_open = mock.mock_open()
+        with mock.patch('fuelclient.commands.task.open', m_open, create=True):
+            self.exec_command(args)
+
+        m_open.assert_called_once_with(expected_path, 'w')
+        m_safe_dump.assert_called_once_with(test_data, mock.ANY,
+                                            default_flow_style=False)
+
+        self.m_get_client.assert_called_once_with('snapshot', mock.ANY)
+        self.m_client.get_default_config.assert_called_once_with()
+
+    def test_task_snapshot_create(self):
+        args = 'task snapshot create'
+        test_data = {}
+        self.exec_command(args)
+
+        self.m_get_client.assert_called_once_with('snapshot', mock.ANY)
+        self.m_client.create_snapshot.assert_called_once_with(test_data)
+
+    @mock.patch('fuelclient.utils.file_exists', return_value=True)
+    def test_task_snapshot_create_w_config_json(self, _):
+        args = 'task snapshot create -c /tmp/snapshot_conf.json'
+        test_data = {'foo': 'bar'}
+        expected_path = '/tmp/snapshot_conf.json'
+
+        m_open = mock.mock_open(read_data=json.dumps(test_data))
+        with mock.patch('fuelclient.commands.task.open', m_open, create=True):
+            self.exec_command(args)
+
+        m_open.assert_called_once_with(expected_path, 'r')
+        self.m_get_client.assert_called_once_with('snapshot', mock.ANY)
+        self.m_client.create_snapshot.assert_called_once_with(test_data)
+
+    @mock.patch('fuelclient.utils.file_exists', return_value=True)
+    def test_task_snapshot_create_w_config_yaml(self, _):
+        args = 'task snapshot create -c /tmp/snapshot_conf.yaml'
+        test_data = {'foo': 'bar'}
+        expected_path = '/tmp/snapshot_conf.yaml'
+
+        m_open = mock.mock_open(read_data=yaml.dump(test_data))
+        with mock.patch('fuelclient.commands.task.open', m_open, create=True):
+            self.exec_command(args)
+
+        m_open.assert_called_once_with(expected_path, 'r')
+        self.m_get_client.assert_called_once_with('snapshot', mock.ANY)
+        self.m_client.create_snapshot.assert_called_once_with(test_data)
 
 
 class TestDeploymentTasksAction(test_engine.BaseCLITest):
