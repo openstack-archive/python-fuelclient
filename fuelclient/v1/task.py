82b4ab44005dfe18d2f698fba866e45abb641b7a
tivaliy <vitaliy@kulanov.org.ua>
2016-08-17 08:22:57 +0300

    Add filters to 'fuel2 task list' command
    Now Nailgun supports filtering transactions by statuses,
    cluster_id and transaction_types. This patch adds filtering
    on client side for 'fuel2 task list' command
    
    DocImpact
    Closes-Bug: 1613674
    
    Change-Id: I8e33944a422a7847e055318b9d73e872244117c7

--- fuelclient/v1/task.py
+++ fuelclient/v1/task.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import six
+
 from fuelclient import objects
 from fuelclient.v1 import base_v1
 
@@ -20,17 +22,46 @@ class TaskClient(base_v1.BaseV1Client):
 
     _entity_wrapper = objects.Task
 
+    def get_all(self, env_id=None, statuses=None, names=None):
+        """Get tasks by specific environment, statuses or names
+
+        :param env_id: Id of specific environment (cluster)
+        :type env_id: int
+        :param statuses: List of string task statuses for filtering
+        :type statuses: list
+        :param names: List of string task names for filtering
+        :type names: list
+        :return: filtered list of tasks
+        :rtype: list
+        """
+        filters = {
+            'cluster_id': env_id,
+            'statuses': statuses,
+            'transaction_types': names
+        }
+        # remove unused filters
+        filters = dict((k, v) for k, v in six.iteritems(filters)
+                       if v is not None)
+        # 'filter': ['param1', 'param2'] --> 'filter': 'param1,param2'
+        for k, v in six.iteritems(filters):
+            if isinstance(v, list):
+                filters[k] = ",".join(s for s in v)
+
+        result = self._entity_wrapper.get_filtered_data(**filters)
+
+        return result
+
     def delete_by_id(self, task_id, force=False):
         """Delete a given task by its id
 
         :param task_id: Id of a task to delete.
         :type task_id: int
         :param force: Force deletion of a task without
-                      concidering its state
+                      considering its state
 
         """
-        env_obj = self._entity_wrapper(obj_id=task_id)
-        env_obj.delete(force=force)
+        task_obj = self._entity_wrapper(obj_id=task_id)
+        task_obj.delete(force=force)
 
 
 def get_client(connection):
