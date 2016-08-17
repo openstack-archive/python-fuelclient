cd96eed0ffd9c2061e2c1bde0feb00b648348de4
tivaliy <vitaliy@kulanov.org.ua>
2016-08-17 08:22:57 +0300

    Add filters to 'fuel2 task list' command
    Now Nailgun supports filtering transactions by statuses,
    cluster_id and transaction_types. This patch adds filtering
    on client side for 'fuel2 task list' command
    
    DocImpact
    Closes-Bug: 1613674
    
    Change-Id: I8e33944a422a7847e055318b9d73e872244117c7

--- fuelclient/tests/unit/v2/lib/test_task.py
+++ fuelclient/tests/unit/v2/lib/test_task.py
@@ -35,17 +35,20 @@ class TestTaskFacade(test_api.BaseLibTest):
 
     def test_task_list(self):
 
-        matcher = self.m_request.get(self.res_uri, json=self.fake_task)
+        matcher = self.m_request.get(self.res_uri, json=self.fake_tasks)
 
         self.client.get_all()
 
         self.assertTrue(self.res_uri, matcher.called)
 
+    def test_task_list_w_parameters(self):
+        pass
+
     def test_task_show(self):
         task_id = 42
         expected_uri = self.get_object_uri(self.res_uri, task_id)
 
-        matcher = self.m_request.get(expected_uri, json=self.fake_tasks)
+        matcher = self.m_request.get(expected_uri, json=self.fake_task)
 
         self.client.get_by_id(task_id)
 
@@ -54,7 +57,7 @@ class TestTaskFacade(test_api.BaseLibTest):
     def test_task_delete(self):
         task_id = 42
         expected_uri = self.get_object_uri(self.res_uri, task_id)
-        matcher = self.m_request.delete(expected_uri, json=self.fake_tasks)
+        matcher = self.m_request.delete(expected_uri, json=self.fake_task)
 
         self.client.delete_by_id(task_id, force=False)
 
@@ -64,7 +67,7 @@ class TestTaskFacade(test_api.BaseLibTest):
     def test_task_delete_force(self):
         task_id = 42
         expected_uri = self.get_object_uri(self.res_uri, task_id)
-        matcher = self.m_request.delete(expected_uri, json=self.fake_tasks)
+        matcher = self.m_request.delete(expected_uri, json=self.fake_task)
 
         self.client.delete_by_id(task_id, force=True)
 
--- fuelclient/v1/task.py
+++ fuelclient/v1/task.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import six
+
 from fuelclient import objects
 from fuelclient.v1 import base_v1
 
@@ -20,17 +22,43 @@ class TaskClient(base_v1.BaseV1Client):
 
     _entity_wrapper = objects.Task
 
+    def get_all(self, env_id=None, statuses=None, types=None):
+        """Get tasks by specific environment, statuses or types
+
+        :param env_id: Id of specific environment (cluster)
+        :type env_id: str
+        :param statuses: List of string task statuses for filtering
+        :type statuses: list
+        :param types: List of string task types for filtering
+        :type types: list
+        :returns: list -- filtered list of tasks
+        """
+        filters = {
+            'cluster_id': env_id,
+            'statuses': statuses,
+            'transaction_types': types
+        }
+        # remove unused filters
+        filters = dict((k, v) for k, v in six.iteritems(filters)
+                       if v is not None)
+        # 'filter': ['param1', 'param2'] --> 'filter': 'param1,param2'
+        for k in filters:
+            filters[k] = ",".join(s for s in filters[k])
+        result = self._entity_wrapper.get_filtered_data(**filters)
+
+        return result
+
     def delete_by_id(self, task_id, force=False):
         """Delete a given task by its id
 
         :param task_id: Id of a task to delete.
         :type task_id: int
         :param force: Force deletion of a task without
-                      concidering its state
+                      considering its state
 
         """
-        env_obj = self._entity_wrapper(obj_id=task_id)
-        env_obj.delete(force=force)
+        task_obj = self._entity_wrapper(obj_id=task_id)
+        task_obj.delete(force=force)
 
 
 def get_client(connection):
