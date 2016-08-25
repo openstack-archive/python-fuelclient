141202be30a747b39faca33df7d31b50aa928eb6
tivaliy <vitaliy@kulanov.org.ua>
2016-08-25 13:39:51 +0300

    Release a new 'fuel2 role *' commands
    Releases a new 'fuel2 role *' commands
    (list, download, update, create, delete)
    instead deprecated ones in old cli:
    
      fuel2 role list
      fuel2 role download
      fuel2 role update
      fuel2 role create
      fuel2 role delete
    
    DocImpact
    
    Change-Id: I2a3a284fa3363097b3b8c8ba8afeb54ce79dcd5c

--- fuelclient/v1/__init__.py
+++ fuelclient/v1/__init__.py
@@ -23,6 +23,7 @@ from fuelclient.v1 import network_group
 from fuelclient.v1 import node
 from fuelclient.v1 import openstack_config
 from fuelclient.v1 import release
+from fuelclient.v1 import role
 from fuelclient.v1 import plugins
 from fuelclient.v1 import task
 from fuelclient.v1 import vip
@@ -40,5 +41,6 @@ __all__ = ('cluster_settings',
            'openstack_config',
            'plugins',
            'release',
+           'role',
            'task',
            'vip')
