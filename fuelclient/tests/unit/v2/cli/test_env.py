bcac21a922f4a8b0ee8b32d2416f89eaf233a8e2
Anastasiia Guzikova <aguzikova@mirantis.com>
2016-08-29 15:37:59 +0300

    Noop run on specified set of nodes
    Closes-bug: #1617993
    Change-Id: I9a8cfca047aebfff94ac0c9966ea5f73b79fc694

--- fuelclient/tests/unit/v2/cli/test_env.py
+++ fuelclient/tests/unit/v2/cli/test_env.py
@@ -130,7 +130,7 @@ class TestEnvCommand(test_engine.BaseCLITest):
         dry_run = False
         noop_run = True
 
-        args = 'env deploy -n'
+        args = 'env deploy --noop'
         args += ' 42'
 
         self.exec_command(args)
@@ -180,7 +180,7 @@ class TestEnvCommand(test_engine.BaseCLITest):
     def test_env_redeploy_noop_run(self):
         dry_run = False
         noop_run = True
-        args = 'env redeploy -n'
+        args = 'env redeploy --noop'
 
         args += ' 42'
 
@@ -242,13 +242,28 @@ class TestEnvCommand(test_engine.BaseCLITest):
     def test_env_nodes_deploy(self):
         env_id = 42
         node_ids = [43, 44]
+        noop_run = False
         args = ('env nodes deploy '
                 '--nodes {n[0]} {n[1]} --env {e}').format(e=env_id, n=node_ids)
 
         self.exec_command(args)
 
         self.m_client.deploy_nodes.return_value = fake_task.get_fake_task()
-        self.m_client.deploy_nodes.assert_called_once_with(env_id, node_ids)
+        self.m_client.deploy_nodes.assert_called_once_with(env_id, node_ids,
+                                                           noop_run=noop_run)
+
+    def test_env_nodes_deploy_noop_run(self):
+        env_id = 42
+        node_ids = [43, 44]
+        noop_run = True
+        args = ('env nodes deploy --noop '
+                '--nodes {n[0]} {n[1]} --env {e}').format(e=env_id, n=node_ids)
+
+        self.exec_command(args)
+
+        self.m_client.deploy_nodes.return_value = fake_task.get_fake_task()
+        self.m_client.deploy_nodes.assert_called_once_with(env_id, node_ids,
+                                                           noop_run=noop_run)
 
     def test_env_nodes_provision(self):
         env_id = 42
