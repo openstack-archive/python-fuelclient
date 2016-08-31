ea66c9ab9a1e921df2929721b0f1b03d238391c2
Alexander Gordeev <agordeev@mirantis.com>
2016-08-31 20:59:21 +0300

    [WIP] Command for setting extensions for cluster
    Change-Id: Idee06633689efece18838766de5c4afcd24190d7
    Implements: blueprint extensions-management
    Partial-Bug: #1614526

---
+++ fuelclient/objects/extension.py
@@ -0,0 +1,41 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from fuelclient.objects.base import BaseObject
+
+
+class Extension(BaseObject):
+
+    class_api_path = "extensions/"
+
+    @property
+    def extensions_url(self):
+        return 'clusters/{0}/extensions'.format(self.environment_id)
+
+    def __init__(self, obj_id, **kwargs):
+        super(Extension, self).__init__(obj_id, **kwargs)
+        self.environment_id = self.obj_id
+
+    def get_env_extensions(self):
+        """Get list of extensions through request to the Nailgun API
+
+        """
+        return self.connection.get_request(self.extensions_url)
+
+    def set_env_extensions(self, extensions):
+        """Enable extensions through request to the Nailgun API
+
+        :param extensions: list of extenstion to be enabled
+        """
+        return self.connection.put_request(self.extensions_url, extensions)
