50390a87e362810ff28b36152cebfde8fc970c2b
slava <vvalyavskiy@mirantis.com>
2016-09-16 02:34:50 +0300

    Add tags support in CLI
    Releases new fuel2 commands to operate on tags:
        fuel2 tag assign
        fuel2 tag create
        fuel2 tag delete
        fuel2 tag download
        fuel2 tag list
        fuel2 tag unassign
        fuel2 tag update
    
    DocImpact
    
    Change-Id: Ib0f48598552f6b066fd375966594cb3a934574e2
    Implements: blueprint role-decomposition

--- fuelclient/main.py
+++ fuelclient/main.py
@@ -18,6 +18,8 @@ import sys
 from cliff import app
 from cliff.commandmanager import CommandManager
 
+from fuelclient.cli import error
+from fuelclient import consts
 from fuelclient import fuelclient_settings
 from fuelclient import utils
 
@@ -32,6 +34,19 @@ class FuelClient(app.App):
     configuration of basic engines.
 
     """
+    is_advanced_mode = None
+
+    @property
+    def is_advanced_mode(self):
+        if self.is_advanced_mode is None:
+            self.is_advanced_mode = False
+            settings = fuelclient_settings.get_settings()
+            try:
+                if settings.MODE == consts.CLIENT_MODES.advanced:
+                    self.is_advanced_mode = True
+            except error.SettingsException:
+                self.is_advanced_mode = False
+        return self.is_advanced_mode
 
     def build_option_parser(self, description, version, argparse_kwargs=None):
         """Overrides default options for backwards compatibility."""
---
+++ fuelclient/objects/tag.py
@@ -0,0 +1,59 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from fuelclient.objects.base import BaseObject
+
+
+class Tag(BaseObject):
+
+    class_api_path = "{}/{}/tags/"
+    instance_api_path = "tags/{}/"
+    assign_api_path = "nodes/{}/tags/"
+
+    def get_tag(self):
+        return self.connection.get_request(
+            self.instance_api_path.format(self.id))
+
+    def update_tag(self, data):
+        return self.connection.put_request(
+            self.instance_api_path.format(self.id), data)
+
+    def delete_tag(self):
+        return self.connection.delete_request(
+            self.instance_api_path.format(self.id))
+
+    @classmethod
+    def get_all(cls, tag_owner, owner_id):
+        return cls.connection.get_request(
+            cls.class_api_path.format(tag_owner, owner_id))
+
+    @classmethod
+    def create_tag(cls, tag_owner, owner_id, data):
+        return cls.connection.post_request(
+            cls.class_api_path.format(tag_owner, owner_id),
+            data)
+
+    @classmethod
+    def assign_tags(cls, tag_ids, node_id):
+        return cls.connection.post_request(
+            cls.assign_api_path.format(node_id), tag_ids)
+
+    @classmethod
+    def unassign_tags(cls, tag_ids, node_id):
+        url = '{0}?tags={1}'.format(
+            cls.assign_api_path.format(node_id),
+            ','.join(map(str, tag_ids))
+        )
+        return cls.connection.delete_request(url)
