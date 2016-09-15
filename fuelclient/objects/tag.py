31ad054a9ab3801ada07d23d7cf1d08ca045d819
slava <vvalyavskiy@mirantis.com>
2016-09-16 02:34:50 +0300

    Add tags support in CLI
    Change-Id: Ib0f48598552f6b066fd375966594cb3a934574e2
    Implements: blueprint role-decomposition
    Depends-On: I390580146048b6e00ec5c42d0adf995a4cff9167

---
+++ fuelclient/objects/tag.py
@@ -0,0 +1,53 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+from fuelclient.objects.base import BaseObject
+
+
+class Tag(BaseObject):
+
+    class_api_path = "tags/"
+    instance_api_path = "tags/{}/"
+    assign_api_path = 'nodes/{node_id}/tags/assign/'
+    unassign_api_path = 'nodes/{node_id}/tags/unassign/'
+
+    @classmethod
+    def get_one(cls, tag_id):
+        return cls.connection.get_request(
+            cls.instance_api_path.format(tag_id))
+
+    @classmethod
+    def upload(cls, tag_id, data):
+        return cls.connection.put_request(
+            cls.instance_api_path.format(tag_id), data)
+
+    @classmethod
+    def create(cls, data):
+        return cls.connection.post_request(cls.class_api_path, data)
+
+    @classmethod
+    def delete_by_id(cls, tag_id):
+        return cls.connection.delete_request(
+            cls.instance_api_path.format(tag_id))
+
+    @classmethod
+    def assign(cls, tag_ids, node_id):
+        return cls.connection.post_request(
+            cls.assign_api_path.format(node_id=node_id), tag_ids)
+
+    @classmethod
+    def unassign(cls, tag_ids, node_id):
+        return cls.connection.post_request(
+            cls.unassign_api_path.format(node_id=node_id), tag_ids)
---
+++ fuelclient/v1/tag.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import six
+
+from fuelclient import objects
+from fuelclient.v1 import base_v1
+
+
+class TagClient(base_v1.BaseV1Client):
+
+    _entity_wrapper = objects.Tag
+
+    def upload(self, data, tag_id):
+        return self._entity_wrapper.upload(tag_id, data)
+
+    def create(self, data, **kwargs):
+        return self._entity_wrapper.create(data)
+
+    def delete_by_id(self, tag_id):
+        return self._entity_wrapper.delete_by_id(tag_id)
+
+    def assign(self, tag_ids, node):
+        return self._entity_wrapper.assign(tag_ids, node)
+
+    def unassign(self, tag_ids, node):
+        return self._entity_wrapper.unassign(tag_ids, node)
+
+
+def get_client(connection):
+    return TagClient(connection)
