4df408db191002732c325360d0f0027e8f51b790
tivaliy <vitaliy@kulanov.org.ua>
2016-11-03 07:39:13 +0200

    Align plugins filtering for 'fuel2 graph list' command
    Change-Id: Iba533f795f78bcd74d5989eff9ed9342c66d05b0
    Closes-Bug: 1638605

--- fuelclient/commands/graph.py
+++ fuelclient/commands/graph.py
@@ -320,7 +320,7 @@ class GraphList(base.BaseListCommand):
             '--plugins',
             dest='filters',
             action='append_const',
-            const='plugins',
+            const='plugin',
             help='Include plugins-specific graphs'
         )
         parser.add_argument(
