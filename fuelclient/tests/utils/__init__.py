8dc51e7dcb6bb2cc74c2e8b4f3f514e38c40cbff
Mikhail <mzhnichkov@mirantis.com>
2016-11-11 13:39:55 +0300

    Add tags support in CLI
    Releases new fuel2 commands to operate on tags:
    fuel2 tag create
    fuel2 tag delete
    fuel2 tag download
    fuel2 tag list
    fuel2 tag update
    
    DocImpact
    
    Change-Id: I71ae78b4a733d83a5441ff0ac5ba627502e7f2b3
    Implements: blueprint role-decomposition
    (cherry picked from commit b482cb522e888c95f670bd857140ec27f63a4df3)

--- fuelclient/tests/utils/__init__.py
+++ fuelclient/tests/utils/__init__.py
@@ -48,6 +48,10 @@ from fuelclient.tests.utils.fake_release import get_fake_releases
 from fuelclient.tests.utils.fake_release import get_fake_attributes_metadata
 from fuelclient.tests.utils.fake_release import get_fake_release_component
 from fuelclient.tests.utils.fake_release import get_fake_release_components
+from fuelclient.tests.utils.fake_role import get_fake_role
+from fuelclient.tests.utils.fake_role import get_fake_roles
+from fuelclient.tests.utils.fake_tag import get_fake_tag
+from fuelclient.tests.utils.fake_tag import get_fake_tags
 
 
 __all__ = (get_fake_deployment_history,
@@ -73,4 +77,6 @@ __all__ = (get_fake_deployment_history,
            get_fake_node_groups,
            get_fake_openstack_config,
            get_fake_plugin,
-           get_fake_plugins)
+           get_fake_plugins,
+           get_fake_tag,
+           get_fake_tags)
